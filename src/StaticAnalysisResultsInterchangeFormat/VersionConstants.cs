e8f4b5fdf72cb5a8a6ad47c5769f6221b920a55f
michaelcfanning <hashtable99@hotmail.com>
2015-12-15 17:08:56 -0800

    Create build and test script. Auto-generate version constants file. Update version to 1.4.7

--- src/StaticAnalysisResultsInterchangeFormat/VersionConstants.cs
+++ src/StaticAnalysisResultsInterchangeFormat/VersionConstants.cs
@@ -1,17 +1,12 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System.Reflection;
-
-[assembly: AssemblyVersion(Microsoft.CodeAnalysis.Sarif.VersionConstants.FileVersion)]
-[assembly: AssemblyFileVersion(Microsoft.CodeAnalysis.Sarif.VersionConstants.FileVersion)]
-
-namespace Microsoft.CodeAnalysis.Sarif
-{
-    public static class VersionConstants
-    {
-        public const string Version = FileVersion + Prerelease;
-        public const string FileVersion = "1.4.6";
-        public const string Prerelease = "-beta";
-    }
-}
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT         
+// license. See LICENSE file in the project root for full license information. 
+namespace Microsoft.CodeAnalysis.Sarif                                         
+{                                                                              
+    public static class VersionConstants                                       
+    {                                                                          
+        public const string Prerelease = "-beta";                       
+        public const string AssemblyVersion = "1.4.7";       
+        public const string FileVersion = AssemblyVersion + ".0";              
+        public const string Version = AssemblyVersion + Prerelease;            
+    }                                                                          
+ }                                                                             
