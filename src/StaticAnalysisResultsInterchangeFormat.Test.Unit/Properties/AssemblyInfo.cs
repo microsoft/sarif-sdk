22e28be3a259a61b00d8320eda1f9f1138c754e2
michaelcfanning <hashtable99@hotmail.com>
2015-12-17 17:08:17 -0800

    1.4.10. Rename in advance of driver utilities repo merge.

--- src/StaticAnalysisResultsInterchangeFormat.Test.Unit/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System.Reflection;
-
-using Microsoft.CodeAnalysis.Sarif;
-
-[assembly: AssemblyTitle("Static Analysis Results Interchange Format Library Unit Tests")]
-
