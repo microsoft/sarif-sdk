9be7ca3d2531dd42470aa7ce9854bef29578c2c8
michaelcfanning <hashtable99@hotmail.com>
2016-02-29 08:37:32 -0800

    Update to publishing script.

--- PublishSignedPackages.cmd
+++ PublishSignedPackages.cmd
@@ -2,11 +2,8 @@
 call SetCurrentVersion.cmd
 
 set VERSION=%MAJOR%.%MINOR%.%PATCH%%PRERELEASE%
-set PACKAGE_ROOT=bld\bin\Nuget\%ID%.%VERSION%
 set NUGET=.nuget\nuget.exe
 set SOURCE=https://nuget.org
-
-echo %PACKAGE_ROOT%
 
 if exist ..\SetNugetSarifKey.cmd (
 call ..\SetNugetSarifKey.cmd
@@ -14,19 +11,24 @@ call %NUGET% SetApiKey %API_KEY% -Source %SOURCE%
 )
 if "%ERRORLEVEL%" NEQ "0" (echo set api key of %API_KEY% to %SOURCE% FAILED && goto Exit)
 
+@REM Publish SDK
 set ID=Sarif.Sdk
+set PACKAGE_ROOT=bld\bin\nuget\%ID%.%VERSION%
+
 call %NUGET% push %PACKAGE_ROOT%.nupkg -Source %SOURCE%
 if "%ERRORLEVEL%" NEQ "0" (echo push to %SOURCE% FAILED && goto Exit)
 
 call %NUGET%push %PACKAGE_ROOT%.symbols.nupkg -Source https://nuget.smbsrc.net/
 if "%ERRORLEVEL%" NEQ "0" (echo push to symsource.org FAILED goto Exit)
 
+@REM Publish Driver
 set ID=Sarif.Driver
-set PACKAGE_ROOT=bld\bin\Nuget\%ID%.%VERSION%
+set PACKAGE_ROOT=bld\bin\nuget\%ID%.%VERSION%
+
 call %NUGET% push %PACKAGE_ROOT%.nupkg -Source %SOURCE%
 if "%ERRORLEVEL%" NEQ "0" (echo push to %SOURCE% FAILED && goto Exit)
 
-call %NUGET% push %PACKAGE_ROOT%.symbols.nupkg -Source https://nuget.smbsrc.net/
-if "%ERRORLEVEL%" NEQ "0" (echo push to symsource.org FAILED goto Exit)
+call %NUGET%push %PACKAGE_ROOT%.symbols.nupkg -Source https://nuget.smbsrc.net/
+if "%ERRORLEVEL%" NEQ "0" (echo push to symbsource.org FAILED goto Exit)
 
 :Exit
