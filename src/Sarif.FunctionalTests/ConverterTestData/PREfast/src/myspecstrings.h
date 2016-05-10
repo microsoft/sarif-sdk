311e897269b0bb164579b62bde7a3195601cf40f
michaelcfanning <hashtable99@hotmail.com>
2016-05-10 16:00:46 -0700

    Remove test files temporarily

--- src/Sarif.FunctionalTests/ConverterTestData/PREfast/src/myspecstrings.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// VISTA SDK specstrings
-#include "specstrings.h"
-// local additions
-#define __analysis_inline __analysis_hint(INLINE)
-#define __analysis_noinline __analysis_hint(NOINLINE)
-
-
--- src/Sarif.FunctionalTests/ConverterTestData/PREfast/src/test021.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory.h>
-
-void f(char *b)
-{
-	char a[10];
-	memcpy(a, b, 11);
-}
-
-void main() { /* dummy */ }
