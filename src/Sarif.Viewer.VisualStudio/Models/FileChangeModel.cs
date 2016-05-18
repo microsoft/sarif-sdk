9a59320ea202c8791c94a54025e4eb5a8f500c00
rtaket <rtaket@microsoft.com>
2016-05-18 16:55:11 -0700

    Added model for Fix.

---
+++ src/Sarif.Viewer.VisualStudio/Models/FileChangeModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Sarif.Viewer.Models
+{
+    public class FileChangeModel : NotifyPropertyChangedObject
+    {
+        private string _filePath;
+        private ObservableCollection<ReplacementModel> _replacements;
+
+        public FileChangeModel()
+        {
+            this._replacements = new ObservableCollection<ReplacementModel>();
+        }
+
+        public string  FilePath
+        {
+            get
+            {
+                return _filePath;
+            }
+            set
+            {
+                if (value != this._filePath)
+                {
+                    _filePath = value;
+
+                    NotifyPropertyChanged("FilePath");
+                    NotifyPropertyChanged("FileName");
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                return Path.GetFileName(this.FilePath);
+            }
+        }
+
+        public ObservableCollection<ReplacementModel> Replacements
+        {
+            get
+            {
+                return this._replacements;
+            }
+        }
+    }
+}
