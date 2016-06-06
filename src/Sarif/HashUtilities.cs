d742fa31429fd9481612f3ad0ed0131382320c74
t-rools <t-rools@HASH3>
2016-06-06 11:20:05 -0700

    CA1021 remove out parameters

--- src/Sarif/HashUtilities.cs
+++ src/Sarif/HashUtilities.cs
@@ -7,11 +7,35 @@ using System.Security.Cryptography;
 
 namespace Microsoft.CodeAnalysis.Sarif
 {
+    public class HashData
+    {
+        string md5;
+        string sha1;
+        string sha256;
+
+        public HashData()
+        {
+            this.md5 = String.Empty;
+            this.sha1 = String.Empty;
+            this.sha256 = String.Empty;
+        }
+
+        public HashData(string md5, string sha1, string sha256)
+        {
+            this.md5 = md5;
+            this.sha1 = sha1;
+            this.sha256 = sha256;
+        }
+
+        public string GetMD5 { get { return md5; } }
+        public string GetSha1 { get { return sha1; } }
+        public string GetSha256 { get { return sha256; } }
+
+    }
     public static class HashUtilities
     {
-        public static void ComputeHashes(string fileName, out string md5, out string sha1, out string sha256)
+        public static HashData ComputeHashes(string fileName)
         {
-            sha1 = sha256 = md5 = null;
             try
             {
                 using (FileStream stream = File.OpenRead(fileName))
@@ -20,26 +44,29 @@ namespace Microsoft.CodeAnalysis.Sarif
                     {
                         var md5Cng = new MD5Cng();
                         byte[] checksum = md5Cng.ComputeHash(bufferedStream);
-                        md5 = BitConverter.ToString(checksum).Replace("-", String.Empty);
+                        string md5 = BitConverter.ToString(checksum).Replace("-", String.Empty);
 
                         stream.Seek(0, SeekOrigin.Begin);
                         bufferedStream.Seek(0, SeekOrigin.Begin);
 
                         var sha1Cng = new SHA1Cng();
                         checksum = sha1Cng.ComputeHash(bufferedStream);
-                        sha1 = BitConverter.ToString(checksum).Replace("-", String.Empty);
+                        string sha1 = BitConverter.ToString(checksum).Replace("-", String.Empty);
 
                         stream.Seek(0, SeekOrigin.Begin);
                         bufferedStream.Seek(0, SeekOrigin.Begin);
 
                         var sha256Cng = new SHA256Cng();
                         checksum = sha256Cng.ComputeHash(bufferedStream);
-                        sha256 = BitConverter.ToString(checksum).Replace("-", String.Empty);                            
+                        string sha256 = BitConverter.ToString(checksum).Replace("-", String.Empty);
+
+                        return new HashData(md5, sha1, sha256);                         
                     }
                 }
             }
             catch (IOException) { }
             catch (UnauthorizedAccessException) { }
+            return null;
         }
 
         public static string ComputeSha256Hash(string fileName)
