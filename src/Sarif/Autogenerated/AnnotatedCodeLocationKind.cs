b45dfe1a3d50a04aaacd869d0a1b1295b5d68ea0
michaelcfanning <hashtable99@hotmail.com>
2016-06-21 09:49:26 -0700

    Add SuppressedExternally to SuppressionStates enum. Release 1.5.25 as non-beta

--- src/Sarif/Autogenerated/AnnotatedCodeLocationKind.cs
+++ src/Sarif/Autogenerated/AnnotatedCodeLocationKind.cs
@@ -1,19 +1,17 @@
 // Copyright (c) Microsoft.  All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.CodeDom.Compiler;
 
 namespace Microsoft.CodeAnalysis.Sarif
 {
     /// <summary>
-    /// A descriptive identifier that categorizes the annotation.
+    /// Values specifying the kind of an annotated code location.
     /// </summary>
-    [Flags]
     [GeneratedCode("Microsoft.Json.Schema.ToDotNet", "0.42.0.0")]
     public enum AnnotatedCodeLocationKind
     {
-        None,
+        Unknown,
         Alias,
         Assignment,
         Branch,
