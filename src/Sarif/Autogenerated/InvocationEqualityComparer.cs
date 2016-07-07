a971415610eb8c9a8a6479742de69265f3fe09e3
Larry Golding <lgolding@microsoft.com>
2016-07-07 09:21:50 -0700

    Support invocation.responseFiles
    Also:
    * Remove a couple of invalid "default" properties which specified that
      the default value for a string-valued property was an empty object.
    * Added "additionalProperties": true to invocation.environmentVariables,
      just for explicitness.

--- src/Sarif/Autogenerated/InvocationEqualityComparer.cs
+++ src/Sarif/Autogenerated/InvocationEqualityComparer.cs
@@ -33,6 +33,28 @@ namespace Microsoft.CodeAnalysis.Sarif
                 return false;
             }
 
+            if (!object.ReferenceEquals(left.ResponseFiles, right.ResponseFiles))
+            {
+                if (left.ResponseFiles == null || right.ResponseFiles == null || left.ResponseFiles.Count != right.ResponseFiles.Count)
+                {
+                    return false;
+                }
+
+                foreach (var value_0 in left.ResponseFiles)
+                {
+                    string value_1;
+                    if (!right.ResponseFiles.TryGetValue(value_0.Key, out value_1))
+                    {
+                        return false;
+                    }
+
+                    if (value_0.Value != value_1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
             if (left.StartTime != right.StartTime)
             {
                 return false;
@@ -75,15 +97,15 @@ namespace Microsoft.CodeAnalysis.Sarif
                     return false;
                 }
 
-                foreach (var value_0 in left.EnvironmentVariables)
+                foreach (var value_2 in left.EnvironmentVariables)
                 {
-                    string value_1;
-                    if (!right.EnvironmentVariables.TryGetValue(value_0.Key, out value_1))
+                    string value_3;
+                    if (!right.EnvironmentVariables.TryGetValue(value_2.Key, out value_3))
                     {
                         return false;
                     }
 
-                    if (value_0.Value != value_1)
+                    if (value_2.Value != value_3)
                     {
                         return false;
                     }
@@ -97,15 +119,15 @@ namespace Microsoft.CodeAnalysis.Sarif
                     return false;
                 }
 
-                foreach (var value_2 in left.Properties)
+                foreach (var value_4 in left.Properties)
                 {
-                    SerializedPropertyInfo value_3;
-                    if (!right.Properties.TryGetValue(value_2.Key, out value_3))
+                    SerializedPropertyInfo value_5;
+                    if (!right.Properties.TryGetValue(value_4.Key, out value_5))
                     {
                         return false;
                     }
 
-                    if (!object.Equals(value_2.Value, value_3))
+                    if (!object.Equals(value_4.Value, value_5))
                     {
                         return false;
                     }
@@ -128,6 +150,22 @@ namespace Microsoft.CodeAnalysis.Sarif
                 if (obj.CommandLine != null)
                 {
                     result = (result * 31) + obj.CommandLine.GetHashCode();
+                }
+
+                if (obj.ResponseFiles != null)
+                {
+                    // Use xor for dictionaries to be order-independent.
+                    int xor_0 = 0;
+                    foreach (var value_6 in obj.ResponseFiles)
+                    {
+                        xor_0 ^= value_6.Key.GetHashCode();
+                        if (value_6.Value != null)
+                        {
+                            xor_0 ^= value_6.Value.GetHashCode();
+                        }
+                    }
+
+                    result = (result * 31) + xor_0;
                 }
 
                 result = (result * 31) + obj.StartTime.GetHashCode();
@@ -156,33 +194,33 @@ namespace Microsoft.CodeAnalysis.Sarif
                 if (obj.EnvironmentVariables != null)
                 {
                     // Use xor for dictionaries to be order-independent.
-                    int xor_0 = 0;
-                    foreach (var value_4 in obj.EnvironmentVariables)
+                    int xor_1 = 0;
+                    foreach (var value_7 in obj.EnvironmentVariables)
                     {
-                        xor_0 ^= value_4.Key.GetHashCode();
-                        if (value_4.Value != null)
+                        xor_1 ^= value_7.Key.GetHashCode();
+                        if (value_7.Value != null)
                         {
-                            xor_0 ^= value_4.Value.GetHashCode();
+                            xor_1 ^= value_7.Value.GetHashCode();
                         }
                     }
 
-                    result = (result * 31) + xor_0;
+                    result = (result * 31) + xor_1;
                 }
 
                 if (obj.Properties != null)
                 {
                     // Use xor for dictionaries to be order-independent.
-                    int xor_1 = 0;
-                    foreach (var value_5 in obj.Properties)
+                    int xor_2 = 0;
+                    foreach (var value_8 in obj.Properties)
                     {
-                        xor_1 ^= value_5.Key.GetHashCode();
-                        if (value_5.Value != null)
+                        xor_2 ^= value_8.Key.GetHashCode();
+                        if (value_8.Value != null)
                         {
-                            xor_1 ^= value_5.Value.GetHashCode();
+                            xor_2 ^= value_8.Value.GetHashCode();
                         }
                     }
 
-                    result = (result * 31) + xor_1;
+                    result = (result * 31) + xor_2;
                 }
             }
 
