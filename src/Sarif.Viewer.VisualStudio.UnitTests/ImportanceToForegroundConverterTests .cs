32302fa9af0beab55e6c0d9c8393afcf5108c68e
Jerica Huang <jerica.huang@gmail.com>
2016-07-18 13:18:39 -0700

    Sorted usings

--- src/Sarif.Viewer.VisualStudio.UnitTests/ImportanceToForegroundConverterTests .cs
+++ src/Sarif.Viewer.VisualStudio.UnitTests/ImportanceToForegroundConverterTests .cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Drawing;
+using System.Globalization;
 using FluentAssertions;
 using Microsoft.CodeAnalysis.Sarif;
 using Microsoft.Sarif.Viewer.Converters;
 using Microsoft.Sarif.Viewer.Models;
-using System.Drawing;
-using System.Globalization;
 using Xunit;
 
 namespace Microsoft.Sarif.Viewer.VisualStudio.UnitTests
