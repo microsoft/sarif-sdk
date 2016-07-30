8734dc44ed34d31fd6fab15c08a19cf499097974
Kevin Schechter <t-kesch@microsoft.com>
2016-07-29 17:18:22 -0700

    F7/F8 traversal

--- src/Sarif.Viewer.VisualStudio/Models/CallTree.cs
+++ src/Sarif.Viewer.VisualStudio/Models/CallTree.cs
@@ -3,16 +3,195 @@
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace Microsoft.Sarif.Viewer.Models
 {
-    public class CallTree
+    public class CallTree : NotifyPropertyChangedObject
     {
+        CallTreeNode _selectedItem;
+        DelegateCommand<TreeView> _selectPreviousCommand;
+        DelegateCommand<TreeView> _selectNextCommand;
+
         public CallTree(IList<CallTreeNode> topLevelNodes)
         {
             TopLevelNodes = new ObservableCollection<CallTreeNode>(topLevelNodes);
         }
 
         public ObservableCollection<CallTreeNode> TopLevelNodes { get; }
+
+        public CallTreeNode SelectedItem
+        {
+            get
+            {
+                return this._selectedItem;
+            }
+            set
+            {
+                this._selectedItem = value;
+                this.NotifyPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        private static int GetIndexInCallTreeNodeList(IList<CallTreeNode> list, CallTreeNode givenNode)
+        {
+            int index = 0;
+            foreach (CallTreeNode listNode in list)
+            {
+                if (listNode == givenNode)
+                {
+                    return index;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            /* this exception should never be thrown, as this method should only be called when 
+             * givenNode is already known to be a member of list 
+             */
+            throw new System.Exception("Given node was not in the list.");
+        }
+
+         private CallTreeNode FindNextNotCall(CallTreeNode currentNode)
+        {
+            CallTreeNode currentParent = currentNode.Parent;
+            if (currentParent == null)
+            {
+                int currentNodeIndexInCallTreeNodeList = GetIndexInCallTreeNodeList(this.TopLevelNodes, currentNode);
+                if (currentNodeIndexInCallTreeNodeList >= this.TopLevelNodes.Count - 1)
+                {
+                    // no next exists, return null and will revert back to currently selected node in original FindNext method
+                    return null;
+                }
+                else
+                {
+                    return this.TopLevelNodes[currentNodeIndexInCallTreeNodeList + 1];
+                }
+            }
+            else
+            {
+                int currentNodeIndexInCallTreeNodeList = GetIndexInCallTreeNodeList(currentParent.Children, currentNode);
+                if (currentNodeIndexInCallTreeNodeList >= currentParent.Children.Count - 1)
+                {
+                    return FindNextNotCall(currentParent);
+                }
+                else
+                {
+                    return currentParent.Children[currentNodeIndexInCallTreeNodeList + 1];
+                }
+            }
+        }
+
+        private CallTreeNode FindNext()
+        {
+            if (this.SelectedItem.Location.Kind == CodeAnalysis.Sarif.AnnotatedCodeLocationKind.Call && this.SelectedItem.Children.Count > 0)
+            {
+                // need to expand selected node
+                return this.SelectedItem.Children[0];
+            }
+            else
+            {
+                CallTreeNode next = FindNextNotCall(this.SelectedItem);
+                if (next == null)
+                {
+                    // no next exists, current remains selected
+                    return this.SelectedItem;
+                }
+                else
+                {
+                    return next; 
+                }
+            }
+        }
+
+        // go to parent, find self, find previous/next, make sure not to roll off
+        private CallTreeNode FindPrevious()
+        {
+            IList<CallTreeNode> currentParentChildren;
+            if (this.SelectedItem.Parent == null)
+            {
+                currentParentChildren = this.TopLevelNodes;
+            }
+            else
+            {
+                currentParentChildren = this.SelectedItem.Parent.Children;
+            }
+
+            CallTreeNode previous = null;
+            foreach (CallTreeNode siblingNode in currentParentChildren)
+            {
+                if (siblingNode == this.SelectedItem)
+                {
+                        if (previous == null)
+                        {
+                            if (this.SelectedItem.Parent == null)
+                            {
+                                // no previous exists, current node stays selected
+                                return this.SelectedItem;
+                            }
+                            else
+                            {
+                                return this.SelectedItem.Parent;
+                            }
+                        }
+                        else
+                        {
+                            if (previous.Location.Kind == CodeAnalysis.Sarif.AnnotatedCodeLocationKind.Call && previous.Children.Count > 0)
+                            {
+                                return previous.Children[previous.Children.Count - 1];
+                            }
+                            else
+                            {
+                                return previous;
+                            }
+                        }
+                }
+                else
+                {
+                    previous = siblingNode;
+                }
+            }
+
+            // default, should not occur
+            return this.SelectedItem;
+        }
+
+        public DelegateCommand<TreeView> SelectPreviousCommand
+        {
+            get
+            {
+                if (_selectPreviousCommand == null)
+                {
+                    _selectPreviousCommand = new DelegateCommand<TreeView>(treeView => {
+                        System.Windows.Controls.TreeView control = treeView as System.Windows.Controls.TreeView;
+                        CallTree model = control.DataContext as CallTree;
+                        model.SelectedItem = FindPrevious();
+                    });
+                }
+
+                return _selectPreviousCommand;
+            }
+        }
+
+
+        public DelegateCommand<TreeView> SelectNextCommand
+        {
+            get
+            {
+                if (_selectNextCommand == null)
+                {
+                    _selectNextCommand = new DelegateCommand<TreeView>(treeView => {
+                        System.Windows.Controls.TreeView control = treeView as System.Windows.Controls.TreeView;
+                        CallTree model = control.DataContext as CallTree;
+                        model.SelectedItem = FindNext();
+                    });
+                }
+
+                return _selectNextCommand;
+            }
+        }
     }
 }
---
+++ src/Sarif.Viewer.VisualStudio/TreeViewHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Sarif.Viewer.Models;
+
+namespace Microsoft.Sarif.Viewer
+{
+    public class TreeViewHelper
+    {
+        private static Dictionary<DependencyObject, TreeViewSelectedItemBehavior> behaviors = new Dictionary<DependencyObject, TreeViewSelectedItemBehavior>();
+
+        public static object GetSelectedItem(DependencyObject obj)
+        {
+            return (object)obj.GetValue(SelectedItemProperty);
+        }
+
+        public static void SetSelectedItem(DependencyObject obj, object value)
+        {
+            obj.SetValue(SelectedItemProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.RegisterAttached("SelectedItem", typeof(object), typeof(TreeViewHelper), new UIPropertyMetadata(new object(), SelectedItemChanged));
+
+        private static void SelectedItemChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(obj is System.Windows.Controls.TreeView))
+                return;
+
+            if (!behaviors.ContainsKey(obj))
+                behaviors.Add(obj, new TreeViewSelectedItemBehavior(obj as System.Windows.Controls.TreeView));
+
+            TreeViewSelectedItemBehavior view = behaviors[obj];
+            view.ChangeSelectedItem(e.NewValue);
+        }
+
+        private class TreeViewSelectedItemBehavior
+        {
+            System.Windows.Controls.TreeView view;
+            public TreeViewSelectedItemBehavior(System.Windows.Controls.TreeView view)
+            {
+                this.view = view;
+                view.SelectedItemChanged += (sender, e) => SetSelectedItem(view, e.NewValue);
+            }
+
+            internal void ChangeSelectedItem(object p)
+            {
+                Stack<CallTreeNode> pathToItem = new Stack<CallTreeNode>();
+                CallTreeNode current = p as CallTreeNode;
+                while (current != null)
+                {
+                    pathToItem.Push(current);
+                    current = current.Parent;
+                }
+
+                int depth = pathToItem.Count;
+                TreeViewItem item = null;
+
+                // gets the path
+                while (pathToItem.Count > 0)
+                {
+                    current = pathToItem.Pop();
+                    if (pathToItem.Count == depth - 1)
+                    {
+                        item = (TreeViewItem)view.ItemContainerGenerator.ContainerFromItem(current);
+                    }
+                    else
+                    {
+                        item = (TreeViewItem)item.ItemContainerGenerator.ContainerFromItem(current);
+                    }
+                }
+
+                if (item != null)
+                {
+                    item.IsSelected = true;
+                }
+            }
+        }
+    }
+}
