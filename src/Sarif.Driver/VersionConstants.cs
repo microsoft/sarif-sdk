9540264daf7aadb9590eb2d798724a6ab84ba709
Larry Golding <lgolding@microsoft.com>
2016-08-30 06:35:26 -0700

    Fix error in SetCurrentVersion.cmd: "developer" => "-developer"

--- src/Sarif.Driver/VersionConstants.cs
+++ src/Sarif.Driver/VersionConstants.cs
@@ -4,8 +4,8 @@ namespace Microsoft.CodeAnalysis.Sarif.Driver
 {                                                                              
     public static class VersionConstants                                       
     {                                                                          
-        public const string Prerelease = "";                       
-        public const string AssemblyVersion = "1.5.29";       
+        public const string Prerelease = "-developer";                       
+        public const string AssemblyVersion = "1.5.30";       
         public const string FileVersion = AssemblyVersion + ".0";              
         public const string Version = AssemblyVersion + Prerelease;            
     }                                                                          
