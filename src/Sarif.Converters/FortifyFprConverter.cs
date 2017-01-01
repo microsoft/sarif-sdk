a2f9bc0375eb532dd59c4b48e2edad871cb1989b
Larry Golding <lgolding@microsoft.com>
2017-01-01 10:33:49 -0800

    Introduce dummy Fortify FPR converter
    It produces a SARIF file with no results.

---
+++ src/Sarif.Converters/FortifyFprConverter.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+
+namespace Microsoft.CodeAnalysis.Sarif.Converters
+{
+    internal class FortifyFprConverter : ToolFileConverterBase
+    {
+        /// <summary>Initializes a new instance of the <see cref="FortifyFprConverter"/> class.</summary>
+        public FortifyFprConverter()
+        {
+        }
+
+        /// <summary>
+        /// Interface implementation for converting a stream in Fortify FPR format to a stream in
+        /// SARIF format.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown when one or more required arguments are null.</exception>
+        /// <param name="input">Stream in Fortify FPR format.</param>
+        /// <param name="output">Stream in SARIF format.</param>
+        public override void Convert(Stream input, IResultLogWriter output)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var tool = new Tool
+            {
+                Name = "Fortify"
+            };
+
+            output.Initialize(id: null, correlationId: null);
+
+            output.WriteTool(tool);
+
+            output.OpenResults();
+            output.CloseResults();
+        }
+    }
+}
