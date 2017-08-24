8f3df18d81438d8436d5b011aad77b3a7182fad4
Vinay Kapadia <v-kapvin@microsoft.com>
2017-08-24 13:44:22 -0700

    Adding tests for SarifErrorListItem LineMarker.

---
+++ src/Sarif.Viewer.VisualStudio.UnitTests/ErrorList/SarifErrorListItemTests.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.CodeAnalysis.Sarif;
+using Xunit;
+
+namespace Microsoft.Sarif.Viewer.VisualStudio.UnitTests
+{
+    public class SarifErrorListItemTests
+    {
+        [Fact]
+        public void SarifErrorListItem_LineMarker_Region_StartLine()
+        {
+            new SarifViewerPackage();
+
+            var item = new SarifErrorListItem
+            {
+                FileName = "file.ext",
+                Region = new Region
+                {
+                    StartLine = 5
+                }
+            };
+
+            var lineMarker = item.LineMarker;
+
+            lineMarker.Should().NotBe(null);
+        }
+
+        [Fact]
+        public void SarifErrorListItem_LineMarker_Region_Offset()
+        {
+            new SarifViewerPackage();
+
+            var item = new SarifErrorListItem
+            {
+                FileName = "file.ext",
+                Region = new Region
+                {
+                    Offset = 20
+                }
+            };
+
+            var lineMarker = item.LineMarker;
+
+            lineMarker.Should().Be(null);
+        }
+
+        [Fact]
+        public void SarifErrorListItem_LineMarker_NoRegion()
+        {
+            new SarifViewerPackage();
+
+            var item = new SarifErrorListItem
+            {
+                FileName = "file.ext"
+            };
+
+            var lineMarker = item.LineMarker;
+
+            lineMarker.Should().Be(null);
+        }
+    }
+}
