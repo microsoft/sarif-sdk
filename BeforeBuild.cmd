58c1753d53608571fda5149a52c9be1a6dafbd43
Jeff Bacon (TEKsystems) <v-jebac@microsoft.com>
2017-10-04 15:16:29 -0700

    Trying to get nuget to work in appveyor

--- BeforeBuild.cmd
+++ BeforeBuild.cmd
@@ -18,8 +18,8 @@ goto ExitFailed
 )
 
 ::Restore nuget packages
-%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio\Sarif.Viewer.VisualStudio.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..
-%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio.UnitTests\Sarif.Viewer.VisualStudio.UnitTests.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..
+%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio\Sarif.Viewer.VisualStudio.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..\
+%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio.UnitTests\Sarif.Viewer.VisualStudio.UnitTests.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..\
 dotnet restore src\Everything.sln
 
 if "%ERRORLEVEL%" NEQ "0" (
