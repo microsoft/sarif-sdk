3612fa6178234346bfc543fca740bc7ab3a9981f
Jeff Bacon (TEKsystems) <v-jebac@microsoft.com>
2017-10-05 10:12:16 -0700

    Try to get nuget packages that appveyor reports as missing

--- BeforeBuild.cmd
+++ BeforeBuild.cmd
@@ -18,6 +18,7 @@ goto ExitFailed
 )
 
 ::Restore nuget packages
+%~dp0.nuget\NuGet.exe restore src\Sarif.ValidationTests\Sarif.ValidationTests.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..\
 %~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio\Sarif.Viewer.VisualStudio.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..\
 %~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio.UnitTests\Sarif.Viewer.VisualStudio.UnitTests.csproj -ConfigFile .nuget\NuGet.Config -SolutionDirectory ..\
 dotnet restore src\Everything.sln
