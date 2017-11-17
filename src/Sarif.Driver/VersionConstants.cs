d2d3f4c7946309c3acc2ac6d52d1ad5f4830d6f4
Larry Golding <lgolding@microsoft.com>
2017-11-17 15:04:19 -0800

    Bump version number

--- src/Sarif.Driver/VersionConstants.cs
+++ src/Sarif.Driver/VersionConstants.cs
@@ -5,7 +5,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Driver
     public static class VersionConstants                                       
     {                                                                          
         public const string Prerelease = "";                       
-        public const string AssemblyVersion = "2.0.0";       
+        public const string AssemblyVersion = "2.1.0";       
         public const string FileVersion = AssemblyVersion + ".0";              
         public const string Version = AssemblyVersion + Prerelease;            
     }                                                                          
