4c101a65b4aa7da68cb51aa95b72a183eb8be4a4
Larry Golding <lgolding@microsoft.com>
2017-11-17 14:39:05 -0800

    Fix #523: Generalize a logged message (#682)
    One of the messages logged by the SDK is specific to BinSkim. Rephrase it to be more generally applicable.

--- src/Sarif/SdkResources.Designer.cs
+++ src/Sarif/SdkResources.Designer.cs
@@ -313,7 +313,7 @@ namespace Microsoft.CodeAnalysis.Sarif {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &apos;{0}&apos; was not evaluated for check &apos;{1}&apos; as the analysis is not relevant based on observed metadata: {2}..
+        ///   Looks up a localized string similar to &apos;{0}&apos; was not evaluated for check &apos;{1}&apos; because the analysis is not relevant for the following reason: {2}..
         /// </summary>
         internal static string NotApplicable_InvalidMetadata {
             get {
