daf262c67c6b87762af3af0ca003618ffd253eb9
Chris Meyer <v-chrmey@microsoft.com>
2017-12-11 13:22:54 -0800

    Fix #705: Viewer: CollectionToVisiblity converters should be combined

--- src/Sarif.Viewer.VisualStudio/Converters/CollectionToVisibilityConverter.cs
+++ src/Sarif.Viewer.VisualStudio/Converters/CollectionToVisibilityConverter.cs
@@ -1,26 +1,24 @@
-﻿// Copyright (c) Microsoft. All rights reserved. 
+﻿
+// Copyright (c) Microsoft. All rights reserved. 
 // Licensed under the MIT license. See LICENSE file in the project root for full license information. 
 
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
 namespace Microsoft.Sarif.Viewer.Converters
 {
-    public class CollectionToVisiblity0Converter : IValueConverter
+    public class CollectionToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var collection = value as ICollection;
+            int val;
 
-            if (collection != null)
+            if (collection != null && int.TryParse(parameter.ToString(), out val))
             {
-                return collection.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+                return collection.Count > val ? Visibility.Visible : Visibility.Collapsed;
             }
             else
             {
