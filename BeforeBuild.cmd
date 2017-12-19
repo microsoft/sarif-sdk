aa969315d57945dbd2fcebfaff156a9261f59960
Chris Meyer <v-chrmey@microsoft.com>
2017-12-19 08:44:32 -0800

    Define symbols for multi-use paths

--- BeforeBuild.cmd
+++ BeforeBuild.cmd
@@ -3,11 +3,14 @@
 @ECHO off
 SETLOCAL
 
+SET NUGET_CONFIG_FILE=%~dp0src\NuGet.Config
+SET NUGET_PACKAGE_DIR=src\packages
+
 ::Restore nuget packages
-%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio\Sarif.Viewer.VisualStudio.csproj -ConfigFile "%~dp0src\NuGet.Config" -OutputDirectory "src\packages"
-%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio.UnitTests\Sarif.Viewer.VisualStudio.UnitTests.csproj -ConfigFile "%~dp0src\NuGet.Config" -OutputDirectory "src\packages"
-%~dp0.nuget\NuGet.exe restore src\Sarif.ValidationTests\Sarif.ValidationTests.csproj -ConfigFile "%~dp0src\NuGet.Config" -OutputDirectory "src\packages"
-dotnet restore src\Everything.sln --packages src\packages
+%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio\Sarif.Viewer.VisualStudio.csproj -ConfigFile "%NUGET_CONFIG_FILE%" -OutputDirectory "%NUGET_PACKAGE_DIR%"
+%~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio.UnitTests\Sarif.Viewer.VisualStudio.UnitTests.csproj -ConfigFile "%NUGET_CONFIG_FILE%" -OutputDirectory "%NUGET_PACKAGE_DIR%"
+%~dp0.nuget\NuGet.exe restore src\Sarif.ValidationTests\Sarif.ValidationTests.csproj -ConfigFile "%NUGET_CONFIG_FILE%" -OutputDirectory "%NUGET_PACKAGE_DIR%"
+dotnet restore src\Everything.sln --packages %NUGET_PACKAGE_DIR%
 
 if "%ERRORLEVEL%" NEQ "0" (
 echo nuget restore failed.
