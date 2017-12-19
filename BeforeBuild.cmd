23331682dc56787e4859ee6be79f444a9221694f
Chris Meyer <v-chrmey@microsoft.com>
2017-12-19 10:28:50 -0800

    Create empty package source bld/bin/nuget before build

--- BeforeBuild.cmd
+++ BeforeBuild.cmd
@@ -6,6 +6,8 @@ SETLOCAL
 SET NuGetConfigFile=%~dp0src\NuGet.Config
 SET NuGetPackageDir=src\packages
 
+md bld\bin\nuget
+
 ::Restore nuget packages
 %~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio\Sarif.Viewer.VisualStudio.csproj -ConfigFile "%NuGetConfigFile%" -OutputDirectory "%NuGetPackageDir%"
 %~dp0.nuget\NuGet.exe restore src\Sarif.Viewer.VisualStudio.UnitTests\Sarif.Viewer.VisualStudio.UnitTests.csproj -ConfigFile "%NuGetConfigFile%" -OutputDirectory "%NuGetPackageDir%"
