24dab24108b280722d27c84480c5f66d7e989b54
Chris Meyer <v-chrmey@microsoft.com>
2018-02-01 13:07:32 -0800

    Removing unnecessary using

--- src/Sarif.Viewer.VisualStudio/ErrorList/ErrorListService.cs
+++ src/Sarif.Viewer.VisualStudio/ErrorList/ErrorListService.cs
@@ -7,7 +7,6 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Windows;
 using EnvDTE;
 using Microsoft.CodeAnalysis.Sarif;
 using Microsoft.CodeAnalysis.Sarif.Converters;
