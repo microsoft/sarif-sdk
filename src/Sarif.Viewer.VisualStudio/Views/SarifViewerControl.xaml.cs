5a738cf2e2a58f22537c329d653d320e43f5dd62
Chris Meyer <v-chrmey@microsoft.com>
2018-02-26 15:19:33 -0800

    Rename CodeLocations to SarifViewerControl

--- src/Sarif.Viewer.VisualStudio/Views/SarifViewerControl.xaml.cs
+++ src/Sarif.Viewer.VisualStudio/Views/SarifViewerControl.xaml.cs
@@ -1,21 +1,16 @@
 ﻿// Copyright (c) Microsoft. All rights reserved. 
 // Licensed under the MIT license. See LICENSE file in the project root for full license information. 
 
-using Microsoft.Sarif.Viewer.Models;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace Microsoft.Sarif.Viewer.Views
 {
     /// <summary>
-    /// Interaction logic for CodeLocations.xaml
+    /// Interaction logic for SarifViewerControl.xaml
     /// </summary>
-    public partial class CodeLocations : UserControl
+    public partial class SarifViewerControl : UserControl
     {
-        public CodeLocations()
+        public SarifViewerControl()
         {
             InitializeComponent();
         }
