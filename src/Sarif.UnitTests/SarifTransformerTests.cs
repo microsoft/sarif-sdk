62247af96c46faec4da2d96032ba58cbc8868061
Chris Meyer <easyrhino@gmail.com>
2018-04-26 10:58:26 -0700

    Transform v1 -> v2 run.logicalLocations

--- src/Sarif.UnitTests/SarifTransformerTests.cs
+++ src/Sarif.UnitTests/SarifTransformerTests.cs
@@ -12,6 +12,16 @@ namespace Microsoft.CodeAnalysis.Sarif
 {
     public class SarifTransformerTests
     {
+        private static SarifLogVersionOne GetSarifLogVersionOne(string logText)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ContractResolver = SarifContractResolverVersionOne.Instance,
+            };
+
+            return JsonConvert.DeserializeObject<SarifLogVersionOne>(logText, settings);
+        }
+
         [Fact]
         public void SarifTransformerTests_ToCurrent_Minimum()
         {
@@ -30,12 +40,7 @@ namespace Microsoft.CodeAnalysis.Sarif
                   ]
                 }";
 
-            JsonSerializerSettings settings = new JsonSerializerSettings()
-            {
-                ContractResolver = SarifContractResolverVersionOne.Instance,
-            };
-
-            SarifLogVersionOne v1Log = JsonConvert.DeserializeObject<SarifLogVersionOne>(v1LogText, settings);
+            SarifLogVersionOne v1Log = GetSarifLogVersionOne(v1LogText);
             var transformer = new SarifVersionOneToCurrentVisitor();
             transformer.VisitSarifLogVersionOne(v1Log);
 
@@ -78,12 +83,7 @@ namespace Microsoft.CodeAnalysis.Sarif
                   ]
                 }";
 
-            JsonSerializerSettings settings = new JsonSerializerSettings()
-            {
-                ContractResolver = SarifContractResolverVersionOne.Instance,
-            };
-
-            SarifLogVersionOne v1Log = JsonConvert.DeserializeObject<SarifLogVersionOne>(v1LogText, settings);
+            SarifLogVersionOne v1Log = GetSarifLogVersionOne(v1LogText);
             var transformer = new SarifVersionOneToCurrentVisitor();
             transformer.VisitSarifLogVersionOne(v1Log);
 
@@ -105,6 +105,65 @@ namespace Microsoft.CodeAnalysis.Sarif
             v2Log.Runs[1].Tool.Should().NotBeNull();
             v2Log.Runs[1].Tool.Name.Should().Be("AssetScanner");
             v2Log.Runs[1].Tool.SemanticVersion.Should().Be("1.7.2");
+        }
+
+        [Fact]
+        public void SarifTransformerTests_ToCurrent_OneRunWithLogicalLocations()
+        {
+            string v1LogText =
+              @"{
+                  ""version"": ""1.0.0"",
+                  ""runs"": [
+                    {
+                      ""logicalLocations"": {
+                        ""collections::list::add"": {
+                          ""name"": ""add"",
+                          ""kind"": ""function"",
+                          ""parentKey"": ""collections::list""
+                        },
+                        ""collections::list"": {
+                          ""name"": ""list"",
+                          ""kind"": ""type"",
+                          ""parentKey"": ""collections""
+                        },
+                        ""collections"": {
+                          ""name"": ""collections"",
+                          ""kind"": ""namespace""
+                        }
+                      },
+                      ""tool"": {
+                        ""name"": ""CodeScanner"",
+                        ""semanticVersion"": ""2.1.0""
+                      },
+                      ""results"": [
+                      ]
+                    }
+                  ]
+                }";
+
+            SarifLogVersionOne v1Log = GetSarifLogVersionOne(v1LogText);
+            var transformer = new SarifVersionOneToCurrentVisitor();
+            transformer.VisitSarifLogVersionOne(v1Log);
+
+            SarifLog v2Log = transformer.SarifLog;
+
+            v2Log.Runs.Should().NotBeNull();
+            v2Log.Runs.Count.Should().Be(1);
+
+            v2Log.Runs[0].LogicalLocations.Should().NotBeNull();
+            v2Log.Runs[0].LogicalLocations.Count.Should().Be(3);
+
+            v2Log.Runs[0].LogicalLocations["collections::list::add"].Name.Should().Be("add");
+            v2Log.Runs[0].LogicalLocations["collections::list::add"].Kind.Should().Be("function");
+            v2Log.Runs[0].LogicalLocations["collections::list::add"].ParentKey.Should().Be("collections::list");
+
+            v2Log.Runs[0].LogicalLocations["collections::list"].Name.Should().Be("list");
+            v2Log.Runs[0].LogicalLocations["collections::list"].Kind.Should().Be("type");
+            v2Log.Runs[0].LogicalLocations["collections::list"].ParentKey.Should().Be("collections");
+
+            v2Log.Runs[0].LogicalLocations["collections"].Name.Should().Be("collections");
+            v2Log.Runs[0].LogicalLocations["collections"].Kind.Should().Be("namespace");
+            v2Log.Runs[0].LogicalLocations["collections"].ParentKey.Should().BeNull();
         }
     }
 }
