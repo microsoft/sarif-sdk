bd89d167702669bb2ca1c8251285a35b5f127cb2
Chris Meyer <easyrhino@gmail.com>
2018-05-16 10:19:39 -0700

    Fix #898 need to URI-encode file paths

--- src/Sarif/VersionConstants.cs
+++ src/Sarif/VersionConstants.cs
@@ -4,7 +4,7 @@ namespace Microsoft.CodeAnalysis.Sarif
 {                                                                              
     public static class VersionConstants                                       
     {                                                                          
-        public const string Prerelease = "alpha.1";                       
+        public const string Prerelease = "-alpha.1";                       
         public const string AssemblyVersion = "2.0.0";       
         public const string FileVersion = "2.0.0" + ".0";    
         public const string Version = AssemblyVersion + Prerelease;            
