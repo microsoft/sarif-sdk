8345db29685210e27a6c42e1daf3d7a7c2575b85
Chris Meyer <easyrhino@gmail.com>
2018-05-31 16:55:07 -0700

    Transform v2 -> v1 stack, stackFrame, region

--- src/Sarif/Visitors/SarifCurrentToVersionOneVisitor.cs
+++ src/Sarif/Visitors/SarifCurrentToVersionOneVisitor.cs
@@ -43,7 +43,8 @@ namespace Microsoft.CodeAnalysis.Sarif.Visitors
                 {
                     InnerExceptions = v2ExceptionData.InnerExceptions?.Select(CreateExceptionData).ToList(),
                     Kind = v2ExceptionData.Kind,
-                    Message = v2ExceptionData.Message
+                    Message = v2ExceptionData.Message,
+                    Stack = CreateStack(v2ExceptionData.Stack)
                 };
             }
 
@@ -196,12 +197,50 @@ namespace Microsoft.CodeAnalysis.Sarif.Visitors
             {
                 physicalLocation = new PhysicalLocationVersionOne
                 {
+                    Region = CreateRegion(v2PhysicalLocation.Region),
                     Uri = v2PhysicalLocation.FileLocation?.Uri,
                     UriBaseId = v2PhysicalLocation.FileLocation?.UriBaseId
                 };
             }
 
             return physicalLocation;
+        }
+
+        internal RegionVersionOne CreateRegion(Region v2Region)
+        {
+            RegionVersionOne region = null;
+
+            if (v2Region != null)
+            {
+                region = CreateRegion(v2Region.StartColumn,
+                                      v2Region.StartLine,
+                                      v2Region.EndColumn,
+                                      v2Region.EndLine,
+                                      v2Region.Length,
+                                      v2Region.Offset);
+            }
+
+            return region;
+        }
+
+        internal RegionVersionOne CreateRegion(int startColumn, int startLine, int endColumn = 0, int endLine = 0, int length = 0, int offset = 0)
+        {
+            RegionVersionOne region = null;
+
+            if (startColumn > 0 || startLine > 0 || endColumn > 0 || endLine > 0 || length > 0 || offset > 0)
+            {
+                region = new RegionVersionOne
+                {
+                    EndColumn = endColumn,
+                    EndLine = endLine,
+                    Length = length,
+                    Offset = offset,
+                    StartColumn = startColumn,
+                    StartLine = startLine
+                };
+            }
+
+            return region;
         }
 
         internal Dictionary<string, string> CreateResponseFilesDictionary(IList<FileLocation> v2ResponseFilesList)
@@ -297,6 +336,59 @@ namespace Microsoft.CodeAnalysis.Sarif.Visitors
             }
 
             return run;
+        }
+
+        internal StackVersionOne CreateStack(Stack v2Stack)
+        {
+            StackVersionOne stack = null;
+
+            if (v2Stack != null)
+            {
+                stack = new StackVersionOne
+                {
+                    Message = v2Stack.Message?.Text,
+                    Properties = v2Stack.Properties,
+                    Frames = v2Stack.Frames?.Select(CreateStackFrame).ToList()
+                };
+            }
+
+            return stack;
+        }
+
+        internal StackFrameVersionOne CreateStackFrame(StackFrame v2StackFrame)
+        {
+            StackFrameVersionOne stackFrame = null;
+
+            if (v2StackFrame != null)
+            {
+                stackFrame = new StackFrameVersionOne
+                {
+                    Address = v2StackFrame.Address,
+                    Module = v2StackFrame.Module,
+                    Offset = v2StackFrame.Offset,
+                    Parameters = v2StackFrame.Parameters,
+                    Properties = v2StackFrame.Properties,
+                    ThreadId = v2StackFrame.ThreadId
+                };
+
+                Location location = v2StackFrame.Location;
+                if (location != null)
+                {
+                    stackFrame.FullyQualifiedLogicalName = location.FullyQualifiedLogicalName;
+                    stackFrame.Message = location.Message?.Text;
+
+                    PhysicalLocation physicalLocation = location.PhysicalLocation;
+                    if (physicalLocation != null)
+                    {
+                        stackFrame.Column = physicalLocation.Region?.StartColumn ?? 0;
+                        stackFrame.Line = physicalLocation.Region?.StartLine ?? 0;
+                        stackFrame.Uri = physicalLocation.FileLocation?.Uri;
+                        stackFrame.UriBaseId = physicalLocation.FileLocation?.UriBaseId;
+                    }
+                }
+            }
+
+            return stackFrame;
         }
 
         internal ToolVersionOne CreateTool(Tool v2Tool)
