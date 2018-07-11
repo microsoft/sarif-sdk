850346c2a5701c045f66c98f902cb3bccea8d0a6
Everett Maus <evmaus@microsoft.com>
2018-07-11 12:46:52 -0700

    Propagate existing result matching properties if we aren't setting them in the result matcher ourselves.

--- src/Sarif/Baseline/ResultMatching/DataStructures/MatchedResults.cs
+++ src/Sarif/Baseline/ResultMatching/DataStructures/MatchedResults.cs
@@ -23,12 +23,18 @@ namespace Microsoft.CodeAnalysis.Sarif.Baseline.ResultMatching
             Result result = null;
 
             Dictionary<string, string> ResultMatchingProperties = new Dictionary<string, string>();
+            Dictionary<string, string> OriginalResultMatchingProperties = null;
             if (BaselineResult != null && CurrentResult != null)
             {
                 result = CurrentResult.Result.DeepClone();
                 result.Id = BaselineResult.Result.Id;
                 result.SuppressionStates = BaselineResult.Result.SuppressionStates;
                 result.BaselineState = BaselineState.Existing;
+
+                if (!BaselineResult.Result.TryGetProperty(ResultMatchingBaseliner.ResultMatchingResultPropertyName, out OriginalResultMatchingProperties))
+                {
+                    OriginalResultMatchingProperties = new Dictionary<string, string>();
+                }
 
                 if (CurrentResult.OriginalRun.Id != null)
                 {
@@ -40,7 +46,12 @@ namespace Microsoft.CodeAnalysis.Sarif.Baseline.ResultMatching
                 result = CurrentResult.Result.DeepClone();
                 result.Id = Guid.NewGuid().ToString();
                 result.BaselineState = BaselineState.New;
-                
+
+                if (!CurrentResult.Result.TryGetProperty(ResultMatchingBaseliner.ResultMatchingResultPropertyName, out OriginalResultMatchingProperties))
+                {
+                    OriginalResultMatchingProperties = new Dictionary<string, string>();
+                }
+
                 if (CurrentResult.OriginalRun.Id != null)
                 {
                     ResultMatchingProperties.Add("Run", CurrentResult.OriginalRun.Id);
@@ -50,7 +61,12 @@ namespace Microsoft.CodeAnalysis.Sarif.Baseline.ResultMatching
             {
                 result = BaselineResult.Result.DeepClone();
                 result.BaselineState = BaselineState.Absent;
-                
+
+                if (!BaselineResult.Result.TryGetProperty(ResultMatchingBaseliner.ResultMatchingResultPropertyName, out OriginalResultMatchingProperties))
+                {
+                    OriginalResultMatchingProperties = new Dictionary<string, string>();
+                }
+
                 if (BaselineResult.OriginalRun.Id != null)
                 {
                     ResultMatchingProperties.Add("Run", BaselineResult.OriginalRun.Id);
@@ -62,8 +78,24 @@ namespace Microsoft.CodeAnalysis.Sarif.Baseline.ResultMatching
                 throw new InvalidOperationException("Cannot generate a Result for a new baseline where both results are null.");
             }
 
+            ResultMatchingProperties = MergeDictionaryPreferFirst(ResultMatchingProperties, OriginalResultMatchingProperties);
+
             result.SetProperty(ResultMatchingBaseliner.ResultMatchingResultPropertyName, ResultMatchingProperties);
 
+            return result;
+        }
+
+        private Dictionary<string, string> MergeDictionaryPreferFirst(Dictionary<string, string> resultMatchingProperties, Dictionary<string, string> originalResultMatchingProperties)
+        {
+            Dictionary<string, string> result = resultMatchingProperties;
+
+            foreach (var key in originalResultMatchingProperties.Keys)
+            {
+                if (!result.ContainsKey(key))
+                {
+                    result[key] = originalResultMatchingProperties[key];
+                }
+            }
             return result;
         }
     }
