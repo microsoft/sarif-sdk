275ad754c8d3f2f28c2b4a035c10e572bd2d9a20
Larry Golding <larrygolding@comcast.net>
2018-07-25 09:24:34 -0700

    Restore validation rule SARIF1013: End column must not be less than start column.

--- src/Sarif.Multitool.FunctionalTests/Update-Expected.ps1
+++ src/Sarif.Multitool.FunctionalTests/Update-Expected.ps1
@@ -1,28 +1,34 @@
 ﻿<#
 .SYNOPSIS
-Update the expected output files for the SarifCli functional tests.
+Update the expected output files for the Sarif.Multitool functional tests.
 
 .DESCRIPTION
-This script runs "SarifCli validate" on each input log file in the SarifCli
+This script runs "Sarif.Multitool validate" on each input log file in the SarifCli
 functional tests TestData directory. For each file foo.sarif, it produces
 an output file foo_Expected.sarif, overwriting any existing file of that
 name in the TestData directory.
 
-.PARAMETERS
-    Configuration
-        The build configuration. Default: Debug
+.PARAMETER Configuration
+    The configuration of the build that produced Sarif.Multitool.exe.
+    Default: Debug
+
+.PARAMETER RuleName
+    The name of the rule whose files are to be regenerated.
+    Default: $null, meaning "all rules"
 #>
 
-param([string] $Configuration = "Debug")
+param(
+    [string] $Configuration = "Debug",
+    [string] $RuleName = $null
+)
 
 $ErrorActionPreference = "Stop"
 
-$SarifExtension = ".sarif"
+Import-Module -Force ..\..\scripts\ScriptUtilities.psm1
+
 $ExpectedSuffix = "_Expected$SarifExtension"
 
-$sdkPath = Join-Path -Resolve $PSScriptRoot "..\.."
-$sarifCliExe = Join-Path $sdkPath "bld\bin\SarifCli\AnyCPU_$Configuration\SarifCli.exe"
-$jsonSchemaPath = Join-Path $sdkPath "src\Sarif\Schemata\Sarif.schema.json"
+$multiToolExePath = "$BinRoot\Sarif.Multitool\AnyCPU_$Configuration\net461\Sarif.Multitool.exe"
 $testDataPath = Join-Path $PSScriptRoot "TestData"
 
 function Test-IsInputFile($fileName) {
@@ -36,7 +42,7 @@ function Get-InputFiles($ruleDirectoryPath) {
 
 function Update-ExpectedFile($inputPath) {
     $outputPath = $inputPath.Replace($SarifExtension, $ExpectedSuffix)
-    & $sarifCliExe validate $inputPath -o $outputPath -j $jsonSchemaPath
+    & $multiToolExePath validate $inputPath --output $outputPath --json-schema $JsonSchemaPath --pretty-print --force --verbose
 }
 
 function Update-RuleDirectory($ruleDirectoryPath) {
@@ -44,7 +50,12 @@ function Update-RuleDirectory($ruleDirectoryPath) {
     $inputFiles | ForEach-Object { Update-ExpectedFile $_ }
 }
 
-$ruleDirectoryNames = Get-ChildItem -Path $testDataPath -Directory | Foreach-Object { $_.Name }
+if ($RuleName -eq $null) {
+    $ruleDirectoryNames = Get-ChildItem -Path $testDataPath -Directory | Foreach-Object { $_.Name }
+} else {
+    $ruleDirectoryNames = @($RuleName)
+}
+
 $ruleDirectoryNames | ForEach-Object {
     $ruleDirectoryPath = [IO.Path]::Combine($testDataPath, $_)
     Update-RuleDirectory $ruleDirectoryPath
