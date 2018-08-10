221e6cd0d5b59273434b7909fb87f9b23ef13be0
Michael C. Fanning <mikefan@microsoft.com>
2018-08-10 11:17:04 -0700

    Do not serialize empty message string dictionaries (#1005)

--- src/Sarif/RuleUtilities.cs
+++ src/Sarif/RuleUtilities.cs
@@ -96,7 +96,8 @@ namespace Microsoft.CodeAnalysis.Sarif
                 dictionary[normalizedResourceName] = resourceValue;
             }
 
-            return dictionary;
+            // We need to return null here, otherwise this empty dictionary will serialize to SARIF logs unnecessarily
+            return dictionary.Count > 0 ? dictionary : null;
         }
 
         public static string NormalizeRuleMessageId(string ruleMessageId, string ruleId, string prefix = null)
