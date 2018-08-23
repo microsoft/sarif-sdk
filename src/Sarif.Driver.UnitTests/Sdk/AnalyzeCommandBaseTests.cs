609a029cbb4609fe626c74f53502bea8b67cbc8f
Michael C. Fanning <mikefan@microsoft.com>
2018-08-23 06:28:05 -0700

    Improve appveyor test output

--- src/Sarif.Driver.UnitTests/Sdk/AnalyzeCommandBaseTests.cs
+++ src/Sarif.Driver.UnitTests/Sdk/AnalyzeCommandBaseTests.cs
@@ -57,6 +57,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Driver
                     TestAnalyzeCommand.SUCCESS : TestAnalyzeCommand.FAILURE;
 
             command.RuntimeErrors.Should().Be(runtimeConditions);
+            command.ExecutionException.ToString().Should().Be("test");
             result.Should().Be(expectedResult, because: command.ExecutionException.ToString());
 
             if (expectedExitReason != ExitReason.None)
