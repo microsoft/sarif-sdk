6e8c139dc4aa59bac571b9f52e46579dd9a67823
Michael C. Fanning <mikefan@microsoft.com>
2018-08-29 08:34:00 -0700

    Remove checked in .NET object model generation utility in preference of NuGet-delivered content. Update Jschema generally to 0.58.0

--- src/Sarif/Autogenerated/SarifNodeKind.cs
+++ src/Sarif/Autogenerated/SarifNodeKind.cs
@@ -8,7 +8,7 @@ namespace Microsoft.CodeAnalysis.Sarif
     /// <summary>
     /// A set of values for all the types that implement <see cref="ISarifNode" />.
     /// </summary>
-    [GeneratedCode("Microsoft.Json.Schema.ToDotNet", "0.56.0.0")]
+    [GeneratedCode("Microsoft.Json.Schema.ToDotNet", "0.58.0.0")]
     public enum SarifNodeKind
     {
         /// <summary>
