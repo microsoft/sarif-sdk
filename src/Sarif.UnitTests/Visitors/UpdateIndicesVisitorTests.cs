0e55d7ce46295f2aef7dad65762a96e0611b4390
Michael C. Fanning <mikefan@microsoft.com>
2019-01-13 20:05:37 -0800

    Delete speculatively needed file

--- src/Sarif.UnitTests/Visitors/UpdateIndicesVisitorTests.cs
+++ src/Sarif.UnitTests/Visitors/UpdateIndicesVisitorTests.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using FluentAssertions;
-using Microsoft.CodeAnalysis.Sarif.Core;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Sarif.Visitors
--- src/Sarif/Visitors/UpdateIndicesVisitor.cs
+++ src/Sarif/Visitors/UpdateIndicesVisitor.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.CodeAnalysis.Sarif.Core;
 
 namespace Microsoft.CodeAnalysis.Sarif.Visitors
 {
