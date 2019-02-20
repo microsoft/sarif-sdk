4a054767452d7d18bc007b7198684579f93e59c1
Michael C. Fanning <mikefan@microsoft.com>
2019-02-20 14:04:47 -0800

    Provide default value of -1 for charOffset.

--- src/Sarif/SarifUtilities.cs
+++ src/Sarif/SarifUtilities.cs
@@ -4,9 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Reflection;
 using System.Resources;
 using System.Text;
 using System.Text.RegularExpressions;
