51e96357c3608ac0d2b2729f4f9436abd020d60e
Harleen Kaur Kohli <hakohli@microsoft.com>
2019-02-28 12:26:51 -0800

    Update logical location kinds (#1313)
    * updating description in schema to include new set
    
    * release md update

--- src/ReleaseHistory.md
+++ src/ReleaseHistory.md
@@ -265,4 +265,4 @@
 ## **v2.0.0-csd.2.beta.2019.02.20** [Sdk](https://www.nuget.org/packages/Sarif.Sdk/2.0.0-csd.2.beta.2019.02.20) | [Driver](https://www.nuget.org/packages/Sarif.Driver/2.0.0-csd.2.beta.2019.02.20) | [Converters](https://www.nuget.org/packages/Sarif.Converters/2.0.0-csd.2.beta.2019.02.20)) | [Multitool](https://www.nuget.org/packages/Sarif.Multitool/2.0.0-csd.2.beta.2019.02.20))
 * API BREAKING: Remove 'invocation.attachments', now replaced by 'run.tool.extensions'. https://github.com/oasis-tcs/sarif-spec/issues/327
 * API BREAKING: Remove 'tool.language' and localization support. https://github.com/oasis-tcs/sarif-spec/issues/325
-
+* API NON-BREAKING: Improve logicalLocation.kind. https://github.com/oasis-tcs/sarif-spec/issues/291
--- src/Sarif/Autogenerated/LogicalLocation.cs
+++ src/Sarif/Autogenerated/LogicalLocation.cs
@@ -61,7 +61,7 @@ namespace Microsoft.CodeAnalysis.Sarif
         public int ParentIndex { get; set; }
 
         /// <summary>
-        /// The type of construct this logical location component refers to. Should be one of 'function', 'member', 'module', 'namespace', 'parameter', 'resource', 'returnType', 'type', or 'variable', if any of those accurately describe the construct.
+        /// The type of construct this logical location component refers to. Should be one of 'function', 'member', 'module', 'namespace', 'parameter', 'resource', 'returnType', 'type', 'variable', 'object', 'array', 'property', 'value', 'element', 'text', 'attribute', 'comment', 'declaration', 'dtd' or 'processingInstruction', if any of those accurately describe the construct.
         /// </summary>
         [DataMember(Name = "kind", IsRequired = false, EmitDefaultValue = false)]
         public string Kind { get; set; }
