1459ee1d0860e3187b1f5b6ae394ccdf971640bf
Michael C. Fanning <mikefan@microsoft.com>
2019-02-28 10:14:20 -0800

    Restore jschema validation (#1307)

---
+++ src/Sarif/Core/ThreadflowLocation.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.CodeAnalysis.Sarif
+{
+    /// <summary>
+    /// A thread flow location of a SARIF thread flow.
+    /// </summary>
+    public partial class ThreadFlowLocation
+    {
+        public bool ShouldSerializeKinds() { return this.Kinds.HasAtLeastOneNonNullValue(); }
+    }
+}
