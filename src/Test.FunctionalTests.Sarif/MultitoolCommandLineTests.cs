e4c0bcc256f96669a8234c6c271be7a0f6afd355
Larry Golding <lgolding@microsoft.com>
2019-05-09 14:17:58 -0700

    #1487: Add functional test to ensure Sarif.Multitool.exe can be launched. (#1489)

---
+++ src/Test.FunctionalTests.Sarif/MultitoolCommandLineTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using System.IO;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.Sarif;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Test.FunctionalTests.Sarif
+{
+    public class MultitoolCommandLineTests
+    {
+        [Fact]
+        [Trait(TestTraits.Bug, "1487")]
+        public void Multitool_LaunchesAndRunsSuccessfully()
+        {
+            string multitoolPath = Path.GetFullPath(
+                Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    @"..\..\Sarif.Multitool\net461\Sarif.Multitool.exe"));
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(multitoolPath, @"validate v2\ConverterTestData\ContrastSecurity\WebGoat.xml.sarif")
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                process.WaitForExit();
+
+                process.ExitCode.Should().Be(0);
+            }
+        }
+    }
+}
