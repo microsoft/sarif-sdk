9dc62b2e31bcb2b6a362cba51fcae637e4f32ec7
Larry Golding <larrygolding@comcast.net>
2019-05-31 11:12:10 -0700

    Fix #1253: location.logicalLocation convenience setter mishandles null

---
+++ src/Test.UnitTests.Sarif/Core/LocationTests.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using Microsoft.CodeAnalysis.Sarif;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.Core
+{
+    public class LocationTests
+    {
+        [Fact]
+        public void Location_LogicalLocation_WhenLogicalLocationsIsAbsent_ReturnsNull()
+        {
+            var location = new Location();
+
+            location.LogicalLocation.Should().BeNull();
+        }
+
+        [Fact]
+        public void Location_LogicalLocation_WhenLogicalLocationsIsPresent_ReturnsFirstArrayElement()
+        {
+            var location = new Location
+            {
+                LogicalLocations = new LogicalLocation[]
+                {
+                    new LogicalLocation
+                    {
+                        FullyQualifiedName = "A.B"
+                    },
+                    new LogicalLocation
+                    {
+                        FullyQualifiedName = "C.D"
+                    }
+                }
+            };
+
+            location.LogicalLocation.FullyQualifiedName.Should().Be("A.B");
+        }
+
+        [Fact]
+        public void Location_LogicalLocation_WhenSetToNonNull_ReplacesLogicalLocationsArray()
+        {
+            var location = new Location
+            {
+                LogicalLocations = new LogicalLocation[]
+                {
+                    new LogicalLocation
+                    {
+                        FullyQualifiedName = "A.B"
+                    },
+                    new LogicalLocation
+                    {
+                        FullyQualifiedName = "C.D"
+                    }
+                }
+            };
+
+            location.LogicalLocation = new LogicalLocation
+            {
+                FullyQualifiedName = "X.Y"
+            };
+
+            location.LogicalLocations.Count.Should().Be(1);
+            location.LogicalLocations[0].FullyQualifiedName.Should().Be("X.Y");
+        }
+
+        [Fact]
+        public void Location_LogicalLocation_WhenSetToNull_RemovesLogicalLocationsArray()
+        {
+            var location = new Location
+            {
+                LogicalLocations = new LogicalLocation[]
+                {
+                    new LogicalLocation
+                    {
+                        FullyQualifiedName = "A.B"
+                    }
+                }
+            };
+
+            location.LogicalLocation = null;
+
+            location.LogicalLocations.Should().BeNull();
+        }
+    }
+}
