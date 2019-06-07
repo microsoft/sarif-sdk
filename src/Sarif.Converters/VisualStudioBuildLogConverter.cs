58f3288022067913bc0c59e3074d9a3d2d4ec41a
Larry Golding <larrygolding@comcast.net>
2019-06-07 13:55:57 -0700

    Introduce VisualStudioBuildLogConverter class and unit test class.

---
+++ src/Sarif.Converters/VisualStudioBuildLogConverter.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+
+namespace Microsoft.CodeAnalysis.Sarif.Converters
+{
+    public class VisualStudioBuildLogConverter : ToolFileConverterBase
+    {
+        public override string ToolName => ToolFormat.VisualStudioBuildLog;
+
+        public override void Convert(Stream input, IResultLogWriter output, OptionallyEmittedData dataToInsert)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Test.UnitTests.Sarif.Converters/VisualStudioBuildLogConverterTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.CodeAnalysis.Sarif.Converters;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.Converters
+{
+    public class VisualStudioBuildLogConverterTests : ConverterTestsBase<VisualStudioBuildLogConverter>
+    {
+        private readonly VisualStudioBuildLogConverter _converter;
+
+        public VisualStudioBuildLogConverterTests()
+        {
+            _converter = new VisualStudioBuildLogConverter();
+        }
+
+        [Fact]
+        public void VisualStudioBuildLogConverter_Exists()
+        {
+            _converter.Should().NotBeNull();
+        }
+    }
+}
