ddc2b0d83fdbe3aedf40f279bd155717939f5afd
Harleen Kaur Kohli <hakohli@microsoft.com>
2019-06-11 14:17:32 -0700

    Fortify severity fix based on Micros focus feedback (#1528)
    * partial fix
    
    * fix with test cases!
    
    * updating test outputs
    
    * rc++
    
    * updated schema version

--- src/Test.UnitTests.Sarif.Converters/FortifyFprConverterTests.cs
+++ src/Test.UnitTests.Sarif.Converters/FortifyFprConverterTests.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using FluentAssertions;
 using Microsoft.CodeAnalysis.Sarif;
+using System.Collections.Generic;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -39,6 +41,60 @@ namespace Microsoft.CodeAnalysis.Sarif.Converters
         public void FortifyFprConverter_Convert_TwoResultsWithNodeRefs()
         {
             RunTest("TwoResultsWithNodeRefs.fpr");
+        }
+
+        [Fact]
+        public void FortifyFprConverter_Convert_ScanWithFailureLevelMatrices()
+        {
+            RunTest("ScanWithFailureLevelMatrices.fpr");
+        }
+
+        [Fact]
+        public void FortifyFprConverter_GetFailureLevelFromRuleMetadata_MissingImpactProperty_ReturnsWarning()
+        {
+            ReportingDescriptor rule = new ReportingDescriptor();
+
+            FailureLevel level = FortifyFprConverter.GetFailureLevelFromRuleMetadata(rule);
+
+            level.Should().Be(FailureLevel.Warning);
+        }
+
+        [Fact]
+        public void FortifyFprConverter_GetFailureLevelFromRuleMetadata_ReturnsAppropriateFailureLevel()
+        {
+            var expectedInputOutputs = new Dictionary<string, FailureLevel>
+            {
+                { "0.0", FailureLevel.Note },
+                { "0.5", FailureLevel.Note },
+                { "1.0", FailureLevel.Note },
+
+                { "1.1", FailureLevel.Warning },
+                { "2.0", FailureLevel.Warning },
+                { "2.5", FailureLevel.Warning },
+                { "2.9", FailureLevel.Warning },
+                { "3.0", FailureLevel.Warning },
+
+                { "3.1", FailureLevel.Error },
+                { "3.5", FailureLevel.Error },
+                { "3.9", FailureLevel.Error },
+                { "4.5", FailureLevel.Error },
+                { "5.0", FailureLevel.Error },
+
+                { "-5.5", FailureLevel.Warning }, //Invalid value, we default it to Warning
+                { "5.5", FailureLevel.Error }, // Invalid value, we guess that it should be treated as Error
+
+            };
+
+            foreach(KeyValuePair<string,FailureLevel> keyValuePair in expectedInputOutputs)
+            {
+                ReportingDescriptor rule = new ReportingDescriptor();
+                rule.SetProperty<string>("Impact", keyValuePair.Key);
+
+                FailureLevel level = FortifyFprConverter.GetFailureLevelFromRuleMetadata(rule);
+
+                level.Should().Be(keyValuePair.Value);
+            }
+            
         }
     }
 }
