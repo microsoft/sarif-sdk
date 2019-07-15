046d33f857275beea38b92e4e55e43c08484677a
Larry Golding <larrygolding@comcast.net>
2019-07-15 15:31:40 -0700

    DRY out validation logic.

--- src/Sarif.Multitool/Rules/SARIF1017.InvalidIndex.cs
+++ src/Sarif.Multitool/Rules/SARIF1017.InvalidIndex.cs
@@ -28,40 +28,48 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
 
         protected override void Analyze(LogicalLocation logicalLocation, string logicalLocationPointer)
         {
-            if (logicalLocation.Index >= 0)
-            {
-                IList<LogicalLocation> logicalLocations = Context.CurrentRun.LogicalLocations;
-                if (!IndexIsValid(logicalLocations, logicalLocation.Index))
-                {
-                    LogResult(
-                        logicalLocationPointer,
-                        nameof(RuleResources.SARIF1017_Default),
-                        "logicalLocation",
-                        "index",
-                        logicalLocation.Index.ToInvariantString(),
-                        "run.logicalLocations",
-                        (logicalLocation.Index + 1).ToInvariantString());
-                }
-            }
+            ValidateArrayIndex(
+                logicalLocation.Index,
+                Context.CurrentRun.LogicalLocations,
+                logicalLocationPointer,
+                "logicalLocation",
+                "index",
+                "run.logicalLocations");
         }
 
         protected override void Analyze(ArtifactLocation artifactLocation, string artifactLocationPointer)
         {
-            IList<Artifact> artifacts = Context.CurrentRun.Artifacts;
-            if (!IndexIsValid(artifacts, artifactLocation.Index))
+            ValidateArrayIndex(
+                artifactLocation.Index,
+                Context.CurrentRun.Artifacts,
+                artifactLocationPointer,
+                "artifactLocation",
+                "index",
+                "run.artifacts");
+        }
+
+        private void ValidateArrayIndex<T>(
+            int index,
+            IList<T> container,
+            string jsonPointer,
+            string objectName,
+            string propertyName,
+            string arrayName)
+        {
+            if (!IndexIsValid(index, container))
             {
                 LogResult(
-                    artifactLocationPointer,
+                    jsonPointer,
                     nameof(RuleResources.SARIF1017_Default),
-                    "artifactLocation",
-                    "index",
-                    artifactLocation.Index.ToInvariantString(),
-                    "run.artifacts",
-                    (artifactLocation.Index + 1).ToInvariantString());
+                    objectName,
+                    propertyName,
+                    index.ToInvariantString(),
+                    arrayName,
+                    (index + 1).ToInvariantString());
             }
         }
 
-        private static bool IndexIsValid<T>(IList<T> container, int index)
-            => index == -1 || (index >= 0 && container?.Count >= index);
+        private static bool IndexIsValid<T>(int index, IList<T> container)
+                => index == -1 || (index >= 0 && container?.Count >= index);
     }
 }
