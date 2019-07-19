2cbb106d4f8fff1fd161acd31b0cc71e444d2593
Larry Golding <lgolding@microsoft.com>
2019-07-19 08:57:46 -0700

    Initial commit of work item filer (#1578)
    * Add WorkItemFiling project; DRY out <LangVersion>.
    
    * Add WorkItemFiling unit test project.

---
+++ src/Test.UnitTests.Sarif.WorkItemFiling/WorkItemFilerTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.WorkItemFiling
+{
+    public class WorkItemFiler
+    {
+        [Fact]
+        public void WorkItemFiler_Exists()
+        {
+            var filer = new WorkItemFiler();
+            filer.Should().NotBeNull();
+        }
+    }
+}
