8d57124043ab1ae90b69cb0ceddb12bce5de460c
Larry Golding <larrygolding@comcast.net>
2019-07-19 11:00:25 -0700

    WorkItemFiler accepts and validates a SARIF log file

--- src/Sarif.WorkItemFiling/WorkItemFiler.cs
+++ src/Sarif.WorkItemFiling/WorkItemFiler.cs
@@ -2,13 +2,21 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.Json.Schema;
+using Microsoft.Json.Schema.Validation;
 
 namespace Microsoft.CodeAnalysis.Sarif.WorkItemFiling
 {
     public class WorkItemFiler
     {
+        private static readonly Validator s_logFileValidator = CreateLogFileValidator();
         private readonly IFileSystem _fileSystem;
-        
+
         /// <summary>
         /// Initializes a new instance of the <see cref="WorkItemFiler"> class.</see>
         /// </summary>
@@ -30,6 +38,63 @@ namespace Microsoft.CodeAnalysis.Sarif.WorkItemFiling
         public void FileWorkItems(string path)
         {
             if (path == null) { throw new ArgumentNullException(nameof(path)); }
+
+            ValidateLogFileAgainstSarifSchema(path);
+        }
+
+        private void ValidateLogFileAgainstSarifSchema(string path)
+        {
+            string logFileContents = _fileSystem.ReadAllText(path);
+
+            // The second argument is a file path that the Validator uses when reporting
+            // errors. The Validator does not attempt to access this file.
+            Result[] errors = s_logFileValidator.Validate(logFileContents, path);
+
+            if (errors?.Length > 0)
+            {
+                throw new ArgumentException(FormatSchemaErrors(path, errors), nameof(path));
+            }
+        }
+
+        private static Validator CreateLogFileValidator()
+        {
+            JsonSchema schema = GetSarifSchema();
+            return new Validator(schema);
+        }
+
+        private static JsonSchema GetSarifSchema()
+        {
+            const string SarifSchemaResource = "Microsoft.CodeAnalysis.Sarif.WorkItemFiling.sarif-2.1.0.json";
+            Assembly thisAssembly = typeof(WorkItemFiler).Assembly;
+
+            string sarifSchemaText;
+            using (Stream sarifSchemaStream = thisAssembly.GetManifestResourceStream(SarifSchemaResource))
+            using (var reader = new StreamReader(sarifSchemaStream))
+            {
+                sarifSchemaText = reader.ReadToEnd();
+            }
+
+            // The second argument is a file path that the SchemaReader uses when reporting
+            // errors. The SchemaReader does not attempt to access this file. Since we obtained
+            // the schema from an embedded resource rather than from a file, we use the name
+            // of the resource.
+            return SchemaReader.ReadSchema(sarifSchemaText, SarifSchemaResource);
+
+        }
+
+        private string FormatSchemaErrors(string path, IEnumerable<Result> errors)
+        {
+            string firstMessageLine = string.Format(CultureInfo.CurrentCulture, Resources.ErrorInvalidSarifLogFile, path);
+
+            var sb = new StringBuilder(firstMessageLine);
+            sb.AppendLine();
+
+            foreach (var error in errors)
+            {
+                sb.AppendLine(error.FormatForVisualStudio(RuleFactory.GetRuleFromRuleId(error.RuleId)));
+            }
+
+            return sb.ToString();
         }
     }
 }
--- src/Test.UnitTests.Sarif.WorkItemFiling/WorkItemFilerTests.cs
+++ src/Test.UnitTests.Sarif.WorkItemFiling/WorkItemFilerTests.cs
@@ -1,21 +1,21 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using FluentAssertions;
 using Microsoft.CodeAnalysis.Sarif;
 using Microsoft.CodeAnalysis.Sarif.WorkItemFiling;
 using Moq;
-using System;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.WorkItemFiling
 {
     public class WorkItemFilerTests
     {
-        private static readonly ResourceExtractor extractor = new ResourceExtractor(typeof(WorkItemFilerTests));
+        private static readonly ResourceExtractor s_extractor = new ResourceExtractor(typeof(WorkItemFilerTests));
 
         [Fact]
-        public void WorkItemFile_RequiresAFileSystem()
+        public void WorkItemFiler_RequiresAFileSystem()
         {
             WorkItemFiler filer;
             Action action = () => filer = new WorkItemFiler(fileSystem: null);
@@ -33,6 +33,57 @@ namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.WorkItemFiling
             Action action = () => filer.FileWorkItems(path: null);
 
             action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void WorkItemFiler_RejectsInvalidSarifFile()
+        {
+            const string LogFilePath = @"Invalid.sarif";
+            string logFileContents = s_extractor.GetResourceText(LogFilePath);
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(x => x.ReadAllText(LogFilePath)).Returns(logFileContents);
+
+            IFileSystem fileSystem = mockFileSystem.Object;
+            var filer = new WorkItemFiler(fileSystem);
+
+            Action action = () => filer.FileWorkItems(LogFilePath);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void WorkItemFiler_AcceptsSarifFileWithNullResults()
+        {
+            const string LogFilePath = @"NullResults.sarif";
+            string logFileContents = s_extractor.GetResourceText(LogFilePath);
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(x => x.ReadAllText(LogFilePath)).Returns(logFileContents);
+
+            IFileSystem fileSystem = mockFileSystem.Object;
+            var filer = new WorkItemFiler(fileSystem);
+
+            Action action = () => filer.FileWorkItems(LogFilePath);
+
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void WorkItemFiler_AcceptsSarifFileWithEmptyResults()
+        {
+            const string LogFilePath = @"EmptyResults.sarif";
+            string logFileContents = s_extractor.GetResourceText(LogFilePath);
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(x => x.ReadAllText(LogFilePath)).Returns(logFileContents);
+
+            IFileSystem fileSystem = mockFileSystem.Object;
+            var filer = new WorkItemFiler(fileSystem);
+
+            Action action = () => filer.FileWorkItems(LogFilePath);
+
+            action.Should().NotThrow();
         }
     }
 }
