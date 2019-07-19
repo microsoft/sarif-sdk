36024cbe374ef5cdb0d92cebae971ffe04512701
Larry Golding <larrygolding@comcast.net>
2019-07-19 09:39:01 -0700

    Introduce FileWorkItems method.

--- src/Sarif.WorkItemFiling/WorkItemFiler.cs
+++ src/Sarif.WorkItemFiling/WorkItemFiler.cs
@@ -1,9 +1,35 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Microsoft.CodeAnalysis.Sarif.WorkItemFiling
 {
     public class WorkItemFiler
     {
+        private readonly IFileSystem _fileSystem;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkItemFiler"> class.</see>
+        /// </summary>
+        /// <param name="fileSystem">
+        /// An object that implements the <see cref="IFileSystem"/> interface, providing
+        /// access to the file system;
+        /// </param>
+        public WorkItemFiler(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+        }
+
+        /// <summary>
+        /// Files work items from the results in a SARIF log file.
+        /// </summary>
+        /// <param name="path">
+        /// The path to the SARIF log file.
+        /// </param>
+        public void FileWorkItems(string path)
+        {
+            if (path == null) { throw new ArgumentNullException(nameof(path)); }
+        }
     }
 }
--- src/Test.UnitTests.Sarif.WorkItemFiling/WorkItemFilerTests.cs
+++ src/Test.UnitTests.Sarif.WorkItemFiling/WorkItemFilerTests.cs
@@ -2,17 +2,37 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using FluentAssertions;
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.CodeAnalysis.Sarif.WorkItemFiling;
+using Moq;
+using System;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.WorkItemFiling
 {
-    public class WorkItemFiler
+    public class WorkItemFilerTests
     {
+        private static readonly ResourceExtractor extractor = new ResourceExtractor(typeof(WorkItemFilerTests));
+
         [Fact]
-        public void WorkItemFiler_Exists()
+        public void WorkItemFile_RequiresAFileSystem()
         {
-            var filer = new WorkItemFiler();
-            filer.Should().NotBeNull();
+            WorkItemFiler filer;
+            Action action = () => filer = new WorkItemFiler(fileSystem: null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void WorkItemFiler_ChecksPathArgument()
+        {
+            var mockFileSystem = new Mock<IFileSystem>();
+            IFileSystem fileSystem = mockFileSystem.Object;
+            var filer = new WorkItemFiler(fileSystem);
+
+            Action action = () => filer.FileWorkItems(path: null);
+
+            action.Should().Throw<ArgumentNullException>();
         }
     }
 }
