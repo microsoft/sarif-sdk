b6c78142d253808c648878ec738e859248df1b84
Larry Golding <larrygolding@comcast.net>
2019-07-22 10:00:33 -0700

    Provide ADO client to ADO filing target.

--- src/Sarif.WorkItemFiling/AzureDevOps/AzureDevOpsFilingTarget.cs
+++ src/Sarif.WorkItemFiling/AzureDevOps/AzureDevOpsFilingTarget.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,6 +12,13 @@ namespace Microsoft.CodeAnalysis.Sarif.WorkItemFiling.AzureDevOps
     /// </summary>
     public class AzureDevOpsFilingTarget : FilingTarget
     {
+        private readonly IAzureDevOpsClient _azureDevOpsClient;
+
+        public AzureDevOpsFilingTarget(IAzureDevOpsClient azureDevOpsClient)
+        {
+            _azureDevOpsClient = azureDevOpsClient ?? throw new ArgumentNullException(nameof(azureDevOpsClient));
+        }
+
         public override Task<IEnumerable<Result>> FileWorkItems(IEnumerable<Result> results)
         {
             throw new System.NotImplementedException();
--- src/Test.UnitTests.Sarif.WorkItemFiling/AzureDevOpsFilingTargetTests.cs
+++ src/Test.UnitTests.Sarif.WorkItemFiling/AzureDevOpsFilingTargetTests.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using FluentAssertions;
 using Microsoft.CodeAnalysis.Sarif.WorkItemFiling.AzureDevOps;
+using Moq;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.WorkItemFiling.AzureDevOps
@@ -10,10 +12,25 @@ namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.WorkItemFiling.AzureDevOps
     public class AzureDevOpsFilingTargetTests
     {
         [Fact]
-        public void AzureDevopsFilingTarget_Exists()
+        public void AzureDevopsFilingTarget_RequiresAzureDevOpsClient()
         {
-            var filingTarget = new AzureDevOpsFilingTarget();
-            filingTarget.Should().NotBeNull();
+            Action action = () => new AzureDevOpsFilingTarget(azureDevOpsClient: null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void AzureDevopsFilingTarget_CanBeCreated()
+        {
+            Action action = () => new AzureDevOpsFilingTarget(azureDevOpsClient: CreateMockAzureDevOpsClient());
+
+            action.Should().NotThrow();
+        }
+
+        private static IAzureDevOpsClient CreateMockAzureDevOpsClient()
+        {
+            var mockAzureDevOpsClient = new Mock<IAzureDevOpsClient>();
+            return mockAzureDevOpsClient.Object;
         }
     }
 }
