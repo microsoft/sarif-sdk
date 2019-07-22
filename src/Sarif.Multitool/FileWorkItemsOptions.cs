c88335859a699d23eb8114ccd756200fcb033b55
Larry Golding <larrygolding@comcast.net>
2019-07-22 10:26:54 -0700

    Add target-type option.

--- src/Sarif.Multitool/FileWorkItemsOptions.cs
+++ src/Sarif.Multitool/FileWorkItemsOptions.cs
@@ -8,5 +8,11 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool
     [Verb("file-work-items", HelpText = "Send SARIF results to a work item tracking system such as GitHub or Azure DevOps")]
     public class FileWorkItemsOptions
     {
+        [Option(
+            't',
+            "target-type",
+            HelpText = "The type of system to which work items are to be filed. Must be one of 'github' and 'azureDevOps'.",
+            Required = true)]
+        public string TargetType { get; internal set; }
     }
 }
