893f9c262ffaccfbae5f3ad8cd3e4959768864d8
Michael C. Fanning <hashtable99@hotmail.com>
2019-07-24 10:58:29 -0700

    Create a shell sample for work items API and add build of this project to get general samples SLN. (#1585)

---
+++ src/Samples/Sarif.WorkItems.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT        
+// license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Sarif.WorkItems.Sample
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
