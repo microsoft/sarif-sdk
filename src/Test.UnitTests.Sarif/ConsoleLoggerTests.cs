94e52c83cc30945c2294d53033d28962105453e8
Michael C. Fanning <michael.fanning@microsoft.com>
2019-10-21 14:38:17 -0700

    Missing pdb location reporting (#1707)
    * Add notification URL to console output if available.
    
    * Update release history.
    
    * Add unit test.
    
    * Fix minor issues observed in PR.

---
+++ src/Test.UnitTests.Sarif/ConsoleLoggerTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.Sarif;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif
+{
+    public class ConsoleLoggerTests
+    {
+        [Fact]
+        public void ConsoleLogger_EmitsNotificationLocationMessageOrToolName()
+        {
+            string messageGuid = Guid.NewGuid().ToString();
+            string toolName = Guid.NewGuid().ToString();
+            string uriGuid = Guid.NewGuid().ToString();
+            Uri uri = new Uri(uriGuid, UriKind.RelativeOrAbsolute);
+
+            var notification = new Notification
+            {
+                Message = new Message
+                {
+                    Text = messageGuid
+                },
+
+                Locations = new[]
+                {
+                    new Location
+                    {
+                         PhysicalLocation = new PhysicalLocation
+                         {
+                             ArtifactLocation = new ArtifactLocation
+                             {
+                                 Uri = uri
+                             }
+                         }
+                    }
+                }
+            };
+
+            // If a notification has one or more locations, the first one should be 
+            // present as part of the console out message.
+            string output = ConsoleLogger.FormatNotificationMessage(notification, toolName);
+            output.Should().Contain(uriGuid);
+            output.Should().NotContain(toolName);
+            output.Should().Contain(messageGuid);
+
+            // In the absence of notificaiton locations, the tool name should be 
+            // present as part of the console out message.
+            notification.Locations = null;
+            output = ConsoleLogger.FormatNotificationMessage(notification, toolName);
+            output.Should().NotContain(uriGuid);
+            output.Should().Contain(toolName);
+            output.Should().Contain(messageGuid);
+        }
+    }
+}
