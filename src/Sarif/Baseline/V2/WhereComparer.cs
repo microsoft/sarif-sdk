b7c0507af8b7801f73d8cabe87edfd08607e2538
Scott Louvau <sclouvau@microsoft.com>
2019-12-09 11:14:04 -0800

    More using fixes.

--- src/Sarif/Baseline/V2/WhereComparer.cs
+++ src/Sarif/Baseline/V2/WhereComparer.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using Microsoft.CodeAnalysis.Sarif.Baseline.ResultMatching;
 
 namespace Microsoft.CodeAnalysis.Sarif.Baseline
