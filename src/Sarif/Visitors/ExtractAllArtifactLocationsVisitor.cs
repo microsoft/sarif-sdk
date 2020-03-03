7a95d9791c1ad866f4e2f7bb803eb5e7716cd97c
nickmarston <55464672+nickmarston@users.noreply.github.com>
2020-03-03 14:51:48 -0800

    Adding ExtractAllLocationUri's visitor (#1794)
    * Add artifiact location extraction visitor.
    
    * Fix testcase names
    
    * Code review feedback.
    
    * Add additional comments and constructor.
    
    * Add additional unit test and respond to review feedback.

---
+++ src/Sarif/Visitors/ExtractAllArtifactLocationsVisitor.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT        
+// license. See LICENSE file in the project root for full license information. 
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.Sarif.Visitors
+{
+    /// <summary>
+    /// A visitor that examines a specified SARIF log to extract all artifact locations for later processing.
+    /// This will extract every location present in the log file, which may then need to be filtered by a
+    /// downstream consumer.
+    /// 
+    /// User may optionally set a specific run before invoking, in order to only fetch artifact locations
+    /// from a subset on an entire SARIF file.
+    /// </summary>
+    public class ExtractAllArtifactLocationsVisitor : SarifRewritingVisitor
+    {
+        private Run CurrentRun { get; set; }
+        public HashSet<ArtifactLocation> AllArtifactLocations { get; private set; }
+
+        public ExtractAllArtifactLocationsVisitor()
+        {
+            AllArtifactLocations = new HashSet<ArtifactLocation>(ArtifactLocation.ValueComparer);
+        }
+
+        public override ArtifactLocation VisitArtifactLocation(ArtifactLocation node)
+        {
+            AllArtifactLocations.Add(node.Resolve(CurrentRun));
+            return node;
+        }
+
+        public override Run VisitRun(Run node)
+        {
+            CurrentRun = node;
+            CurrentRun = base.VisitRun(node);
+            return node;
+        }
+    }
+}
