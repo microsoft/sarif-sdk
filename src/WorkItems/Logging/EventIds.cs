5a7c9f5d31132d93e9aa533ed2e15bcf1a8d9931
Ryley Taketa <rtaket@microsoft.com>
2020-04-02 09:51:34 -0700

    Add sentinel value for empty or null strings.

--- src/WorkItems/Logging/EventIds.cs
+++ src/WorkItems/Logging/EventIds.cs
@@ -7,6 +7,8 @@ namespace Microsoft.WorkItems.Logging
 {
     public static class EventIds
     {
+        public static EventId None => new EventId(0, nameof(None));
+
         public static EventId LogsToProcessMetrics => new EventId(9001, nameof(LogsToProcessMetrics));
         public static EventId WorkItemFiledCoreMetrics => new EventId(9002, nameof(WorkItemFiledCoreMetrics));
         public static EventId WorkItemFiledDetailMetrics => new EventId(9003, nameof(WorkItemFiledDetailMetrics));
--- src/WorkItems/Logging/LoggerExtensions.cs
+++ src/WorkItems/Logging/LoggerExtensions.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using Octokit;
 
 namespace Microsoft.WorkItems.Logging
 {
@@ -24,6 +26,46 @@ namespace Microsoft.WorkItems.Logging
                 eventId, 
                 new MetricsLogValues(message, eventId, customDimensions),
                 exception: null, 
+                (state, error) => state.ToString());
+        }
+
+        public static void LogDebug(this ILogger logger, string message, IDictionary<string, object> customDimensions, Exception exception = null)
+        {
+            LogMessage(logger, LogLevel.Debug, message, customDimensions, exception);
+        }
+
+        public static void LogInformation(this ILogger logger, string message, IDictionary<string, object> customDimensions, Exception exception = null)
+        {
+            LogMessage(logger, LogLevel.Information, message, customDimensions, exception);
+        }
+
+        public static void LogWarning(this ILogger logger, string message, IDictionary<string, object> customDimensions, Exception exception = null)
+        {
+            LogMessage(logger, LogLevel.Warning, message, customDimensions, exception);
+        }
+
+        public static void LogError(this ILogger logger, string message, IDictionary<string, object> customDimensions, Exception exception = null)
+        {
+            LogMessage(logger, LogLevel.Error, message, customDimensions, exception);
+        }
+
+        public static void LogCritical(this ILogger logger, string message, IDictionary<string, object> customDimensions, Exception exception = null)
+        {
+            LogMessage(logger, LogLevel.Critical, message, customDimensions, exception);
+        }
+
+        private static void LogMessage(this ILogger logger, LogLevel logLevel, string message, IDictionary<string, object> customDimensions, Exception exception = null)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            logger.Log(
+                logLevel,
+                EventIds.None,
+                new MetricsLogValues(message, EventIds.None, customDimensions),
+                exception,
                 (state, error) => state.ToString());
         }
     }
