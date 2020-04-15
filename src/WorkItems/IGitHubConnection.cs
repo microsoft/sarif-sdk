02d3c50f79141e7d7c641f2e9345555dd3cd3ba2
Nicholas Marston <nmarston@microsoft.com>
2020-04-15 13:45:00 -0700

    Code review feedback. Split out GitHub and Ado mock clients.

--- src/WorkItems/IGitHubConnection.cs
+++ src/WorkItems/IGitHubConnection.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 namespace Microsoft.WorkItems
 {
     /// <summary>
-    /// This interface allows for mocking of the low-level Gitjub connection class.
+    /// This interface allows for mocking of the low-level Github connection class.
     /// </summary>
     internal interface IGitHubConnection
     {
         /// <summary>
         /// Provide for the instantiation of the GitHub connection instance.
         /// </summary>
-        /// <param name="organization">The AzureDevOps account URI for the connection.</param>
+        /// <param name="organization">The GitHuub organization URI for the connection.</param>
         /// <param name="personalAccessToken">A personal access token with sufficient permissions to establish the connection.</param>
         /// <returns></returns>
         Task<IGitHubClientWrapper> ConnectAsync(string organization, string personalAccessToken);
