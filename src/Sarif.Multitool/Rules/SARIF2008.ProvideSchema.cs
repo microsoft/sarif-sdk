54df61d8245b1cfae36d92d476e9bd6e530e5b09
Harleen Kaur Kohli <erferferfg>
2020-06-19 14:55:24 -0700

    reviews++

--- src/Sarif.Multitool/Rules/SARIF2008.ProvideSchema.cs
+++ src/Sarif.Multitool/Rules/SARIF2008.ProvideSchema.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Json.Pointer;
-using System;
 using System.Collections.Generic;
 
 namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
@@ -14,7 +12,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
             Text = RuleResources.SARIF2008_ProvideSchema_FullDescription_Text
         };
 
-        public override FailureLevel DefaultLevel => FailureLevel.Error;
+        public override FailureLevel DefaultLevel => FailureLevel.Warning;
 
         public override string Id => RuleId.ProvideSchema;
 
@@ -25,15 +23,9 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
 
         protected override void Analyze(SarifLog log, string logPointer)
         {
-            AnalyzeSchema(log.SchemaUri, logPointer);
-        }
-
-        private void AnalyzeSchema(Uri schemaUri, string pointer)
-        {
-            if (schemaUri == null)
+            if (!Context.InputLogToken.HasProperty("$schema"))
             {
-                LogResult(pointer, nameof(RuleResources.SARIF2008_ProvideSchema_Warning_Default_Text));
-                return;
+                LogResult(logPointer, nameof(RuleResources.SARIF2008_ProvideSchema_Warning_Default_Text));
             }
         }
     }
