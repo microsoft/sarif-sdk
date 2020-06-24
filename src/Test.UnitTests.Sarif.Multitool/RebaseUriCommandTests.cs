a1c3eab365f36b34c0bacd9e90173dfa493a8368
Michael C. Fanning <michael.fanning@microsoft.com>
2020-06-24 06:35:56 -0700

    Honor insert and remove arguments for rebase uri command. (#1927)
    * Add data insert/removal to rebase uri command.
    
    * Update release notes for rebase uri command.
    
    * Remove console message.

--- src/Test.UnitTests.Sarif.Multitool/RebaseUriCommandTests.cs
+++ src/Test.UnitTests.Sarif.Multitool/RebaseUriCommandTests.cs
@@ -1,10 +1,17 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.IO;
 using System.Text;
 using FluentAssertions;
+
+using Microsoft.Extensions.Options;
+
 using Moq;
+
+using Newtonsoft.Json;
+
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,16 +20,95 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool
     public class RebaseUriCommandTests : FileDiffingUnitTests
     {
         private static readonly ResourceExtractor Extractor = new ResourceExtractor(typeof(RebaseUriCommandTests));
+
         private RebaseUriOptions options;
 
         public RebaseUriCommandTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
+
+        [Fact]
+        public void RebaseUriCommand_InjectsRegions()
+        {
+            string productDirectory = FileDiffingFunctionalTests.GetProductDirectory();
+            string analysisFile = Path.Combine(productDirectory, @"ReleaseHistory.md");
+            File.Exists(analysisFile).Should().BeTrue();
+
+            var sarifLog = new SarifLog
+            {
+                Runs = new[]
+                {
+                    new Run { Results = new[] { 
+                        new Result { Locations = new [] {
+                               new Location {
+                                    PhysicalLocation = new PhysicalLocation {
+                                         Region = new Region { StartLine = 7 },
+                                         ArtifactLocation = new ArtifactLocation
+                                         {
+                                             Uri = new Uri(analysisFile)
+                                         }
+                                   }
+                               } }
+                    } } }
+                }
+            };
+
+            string inputSarifLog = JsonConvert.SerializeObject(sarifLog);
+
+            string logFilePath = @"c:\logs\mylog.sarif";
+            StringBuilder transformedContents = new StringBuilder();
+
+            RebaseUriOptions options = CreateDefaultOptions();
+
+            options.TargetFileSpecifiers = new string[] { logFilePath };
+            
+            options.DataToInsert = new[] 
+            { 
+                OptionallyEmittedData.RegionSnippets |
+                OptionallyEmittedData.ContextRegionSnippets 
+            };
+
+            Mock<IFileSystem> mockFileSystem = ArrangeMockFileSystem(inputSarifLog, logFilePath, transformedContents);
+
+            // Test snippet injection.
+            var rebaseUriCommand = new RebaseUriCommand(mockFileSystem.Object);
+
+            int returnCode = rebaseUriCommand.Run(options);
+            returnCode.Should().Be(0);
+
+            SarifLog actualLog = JsonConvert.DeserializeObject<SarifLog>(transformedContents.ToString());
+            actualLog.Runs[0].Results[0].Locations[0].PhysicalLocation.Region.Snippet.Should().NotBeNull();
+            actualLog.Runs[0].Results[0].Locations[0].PhysicalLocation.ContextRegion.Snippet.Should().NotBeNull();
+
+            // Now test that this data is removed.
+            inputSarifLog = JsonConvert.SerializeObject(actualLog);
+            transformedContents.Length = 0;
+            mockFileSystem = ArrangeMockFileSystem(inputSarifLog, logFilePath, transformedContents);
+            rebaseUriCommand = new RebaseUriCommand(mockFileSystem.Object);
+
+            options.DataToRemove = options.DataToInsert;
+            options.DataToInsert = null;
+
+            returnCode = rebaseUriCommand.Run(options);
+            returnCode.Should().Be(0);
+
+            actualLog = JsonConvert.DeserializeObject<SarifLog>(transformedContents.ToString());
+            actualLog.Runs[0].Results[0].Locations[0].PhysicalLocation.Region.Snippet.Should().BeNull();
+            actualLog.Runs[0].Results[0].Locations[0].PhysicalLocation.ContextRegion.Snippet.Should().BeNull();
+        }
+
 
         [Fact]
         public void RebaseUriCommand_RebaseRunWithArtifacts()
         {
             string testFilePath = "RunWithArtifacts.sarif";
 
-            this.options = new RebaseUriOptions
+            this.options = CreateDefaultOptions();
+
+            RunTest(testFilePath);
+        }
+
+        private static RebaseUriOptions CreateDefaultOptions()
+        {
+            return new RebaseUriOptions
             {
                 BasePath = @"C:\vs\src\2\s\",
                 BasePathToken = "SRCROOT",
@@ -30,8 +116,6 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool
                 SarifOutputVersion = SarifVersion.Current,
                 PrettyPrint = true
             };
-
-            RunTest(testFilePath);
         }
 
         protected override string ConstructTestOutputFromInputResource(string testFilePath, object parameter)
