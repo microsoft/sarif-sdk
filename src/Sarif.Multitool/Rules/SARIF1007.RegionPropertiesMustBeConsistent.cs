3de8545298802501b8ef082bcb10a506f7a288cc
Harleen Kaur Kohli <hakohli@microsoft.com>
2020-06-24 16:41:38 -0700

    sarif1007 subrule: RegionStartPropertyMustBePresent (#1935)
    * changing file formatting per convention
    
    * adding sub-rule: RegionStartPropertyMustBePresent
    
    Co-authored-by: Harleen Kaur Kohli <erferferfg>

--- src/Sarif.Multitool/Rules/SARIF1007.RegionPropertiesMustBeConsistent.cs
+++ src/Sarif.Multitool/Rules/SARIF1007.RegionPropertiesMustBeConsistent.cs
@@ -2,38 +2,54 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+
 using Microsoft.Json.Pointer;
+
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
 {
     public class RegionPropertiesMustBeConsistent : SarifValidationSkimmerBase
     {
-        public override MultiformatMessageString FullDescription => new MultiformatMessageString
-        {
-            Text = RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_FullDescription_Text
-        };
-
-        public override FailureLevel DefaultLevel => FailureLevel.Error;
-
+        /// <summary>
+        /// SARIF1007
+        /// </summary>
         public override string Id => RuleId.RegionPropertiesMustBeConsistent;
 
-        protected override IEnumerable<string> MessageResourceNames => new string[]
-        {
-            nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_EndLineMustNotPrecedeStartLine_Text),
-            nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_EndColumnMustNotPrecedeStartColumn_Text)
-        };
+        /// <summary>
+        /// Placeholder_SARIF1007_RegionPropertiesMustBeConsistent_FullDescription_Text
+        /// </summary>
+        public override MultiformatMessageString FullDescription => new MultiformatMessageString { Text = RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_FullDescription_Text };
+
+        protected override IEnumerable<string> MessageResourceNames => new string[] {
+                    nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_EndLineMustNotPrecedeStartLine_Text),
+                    nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_EndColumnMustNotPrecedeStartColumn_Text),
+                    nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_RegionStartPropertyMustBePresent_Text)
+                };
+
+        public override FailureLevel DefaultLevel => FailureLevel.Error;
 
         protected override void Analyze(Region region, string regionPointer)
         {
             var jsonPointer = new JsonPointer(regionPointer);
             JToken regionToken = jsonPointer.Evaluate(Context.InputLogToken);
 
+            if (!region.IsBinaryRegion &&
+                !region.IsLineColumnBasedTextRegion &&
+                !region.IsOffsetBasedTextRegion)
+            {
+                // {0}: Placeholder_SARIF1007_RegionPropertiesMustBeConsistent_Error_RegionStartPropertyMustBePresent_Text
+                LogResult(
+                    regionPointer,
+                    nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_RegionStartPropertyMustBePresent_Text));
+            }
+
             if (regionToken.HasProperty(SarifPropertyName.EndLine) &&
                 region.EndLine < region.StartLine)
             {
                 string endLinePointer = regionPointer.AtProperty(SarifPropertyName.EndLine);
 
+                // {0}: The value of the "endLine" property is {1}, which is less than the value of the "startLine" property, which is {2}.
                 LogResult(
                     endLinePointer,
                     nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_EndLineMustNotPrecedeStartLine_Text),
@@ -48,6 +64,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
             {
                 string endColumnPointer = regionPointer.AtProperty(SarifPropertyName.EndColumn);
 
+                // {0}: The value of the "endColumn" property is {1}, which is less than the value of the "startColumn" property, which is {2}.
                 LogResult(
                     endColumnPointer,
                     nameof(RuleResources.SARIF1007_RegionPropertiesMustBeConsistent_Error_EndColumnMustNotPrecedeStartColumn_Text),
