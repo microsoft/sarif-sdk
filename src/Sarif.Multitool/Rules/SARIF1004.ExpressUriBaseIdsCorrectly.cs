5d7ad6d1ebc6a60e6e5356a651fd677f40c4ae18
Harleen Kaur Kohli <erferferfg>
2020-06-24 12:01:06 -0700

    changing file contents to follow conventions

--- src/Sarif.Multitool/Rules/SARIF1004.ExpressUriBaseIdsCorrectly.cs
+++ src/Sarif.Multitool/Rules/SARIF1004.ExpressUriBaseIdsCorrectly.cs
@@ -9,26 +9,31 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
 {
     public class ExpressUriBaseIdsCorrectly : SarifValidationSkimmerBase
     {
-        public override MultiformatMessageString FullDescription => new MultiformatMessageString
-        {
-            Text = RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_FullDescription_Text
-        };
+        /// <summary>
+        /// SARIF1004
+        /// </summary>
+        public override string Id => RuleId.ExpressUriBaseIdsCorrectly;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override MultiformatMessageString FullDescription => new MultiformatMessageString { Text = RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_FullDescription_Text };
+
+        protected override IEnumerable<string> MessageResourceNames => new string[] {
+                    nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdRequiresRelativeUri_Text),
+                    nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_TopLevelUriBaseIdMustBeAbsolute_Text),
+                    nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdValueMustEndWithSlash_Text)
+                };
 
         public override FailureLevel DefaultLevel => FailureLevel.Error;
 
-        public override string Id => RuleId.ExpressUriBaseIdsCorrectly;
-
-        protected override IEnumerable<string> MessageResourceNames => new string[]
-        {
-            nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdRequiresRelativeUri_Text),
-            nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_TopLevelUriBaseIdMustBeAbsolute_Text),
-            nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdValueMustEndWithSlash_Text)
-        };
-
         protected override void Analyze(ArtifactLocation fileLocation, string fileLocationPointer)
         {
+            // UriBaseIdRequiresRelativeUri: The 'uri' property of 'fileLocation' must be a relative uri, since 'uriBaseId' is present.
             if (fileLocation.UriBaseId != null && fileLocation.Uri.IsAbsoluteUri)
             {
+                // {0}: This fileLocation object contains a "uriBaseId" property, which means that the value
+                // of the "uri" property must be a relative URI reference, but "{1}" is an absolute URI reference.
                 LogResult(
                     fileLocationPointer.AtProperty(SarifPropertyName.Uri),
                     nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdRequiresRelativeUri_Text),
@@ -63,14 +68,26 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
             {
                 var uri = new Uri(uriString, UriKind.RelativeOrAbsolute);
 
+                // TopLevelUriBaseIdMustBeAbsolute: TODO
                 if (artifactLocation.UriBaseId == null && !uri.IsAbsoluteUri)
                 {
-                    LogResult(pointer, nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_TopLevelUriBaseIdMustBeAbsolute_Text), uriString, uriBaseId);
+                    // {0}: The URI '{1}' belonging to the '{2}' element of run.originalUriBaseIds is not an absolute URI.
+                    LogResult(
+                        pointer,
+                        nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_TopLevelUriBaseIdMustBeAbsolute_Text),
+                        uriString,
+                        uriBaseId);
                 }
 
+                // UriBaseIdValueMustEndWithSlash: TODO
                 if (!uriString.EndsWith("/"))
                 {
-                    LogResult(pointer, nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdValueMustEndWithSlash_Text), uriString, uriBaseId);
+                    // {0}: The URI '{1}' belonging to the '{2}' element of run.originalUriBaseIds does not end with a slash.
+                    LogResult(
+                        pointer,
+                        nameof(RuleResources.SARIF1004_ExpressUriBaseIdsCorrectly_Error_UriBaseIdValueMustEndWithSlash_Text),
+                        uriString,
+                        uriBaseId);
                 }
             }
         }
