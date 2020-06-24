bf41752c304379f0b7038499a86723f18dc205cb
Scott Louvau <sclouvau@microsoft.com>
2020-06-24 16:51:15 -0700

    Demo scripts

---
+++ src/Samples/BSOA.Demo/Demo.Big.cmd
@@ -0,0 +1,38 @@
+@ECHO OFF
+SETLOCAL
+
+SET SampleFolderPath=C:\Download\Demo\V2
+SET SampleFileName=CodeAsData.50M
+
+SET SarifToCsv="%~dp0..\SarifToCsv\bin\Release\netcoreapp3.1\SarifToCsv.exe"
+SET SarifToCsv.Old="%~dp0bin\SarifToCsv_SDK_v2.0\SarifToCsv.exe"
+
+SET SarifMultitool="%~dp0..\..\..\bld\bin\AnyCPU_Release\Sarif.Multitool\netcoreapp3.0\Sarif.Multitool.exe"
+SET SarifMultitool.Old="%~dp0bin\SarifMultitool_SDK_v2.3.1\Sarif.Multitool.exe"
+
+CALL :setESC
+
+ECHO.
+ECHO %ESC%[31mOLD: SarifToCsv %SampleFolderPath%\Inputs\%SampleFileName%.sarif %ESC%[0m
+%SarifToCsv.Old% %SampleFolderPath%\Inputs\%SampleFileName%.sarif
+
+ECHO.
+ECHO %ESC%[92mNEW: SarifToCsv %SampleFolderPath%\Out\%SampleFileName%.bsoa %ESC%[0m
+%SarifToCsv% %SampleFolderPath%\Out\%SampleFileName%.bsoa
+
+ECHO.
+ECHO %ESC%[31mOLD: Sarif.Multitool query %SampleFolderPath%\Inputs\%SampleFileName%.sarif -e "ruleId : 433" %ESC%[0m
+%SarifMultitool.Old% query %SampleFolderPath%\Inputs\%SampleFileName%.sarif -e "ruleId : 433"
+
+ECHO.
+ECHO %ESC%[92mNEW: Sarif.Multitool query %SampleFolderPath%\Out\%SampleFileName%.bsoa -e "ruleId : 433" %ESC%[0m
+%SarifMultitool% query %SampleFolderPath%\Out\%SampleFileName%.bsoa -e "ruleId : 433"
+
+ECHO %ESC%[0m
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
+exit /B 0
--- src/Samples/BSOA.Demo/Demo.cmd
+++ src/Samples/BSOA.Demo/Demo.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 SET SampleFolderPath=C:\Download\Demo\V2
-SET SampleFilePath=%SampleFolderPath%\Inputs\CodeAsData.20000.sarif
-SET SampleOutBsoaPath=%SampleFolderPath%\Out\CodeAsData.20000.bsoa
-SET SampleOutJsonPath=%SampleFolderPath%\Out\CodeAsData.20000.sarif
+SET SampleFilePath=%SampleFolderPath%\Inputs\CodeAsData.50M.sarif
+SET SampleOutBsoaPath=%SampleFolderPath%\Out\CodeAsData.50M.bsoa
+SET SampleOutJsonPath=%SampleFolderPath%\Out\CodeAsData.50M.sarif
 
 CLS
 PUSHD "%~dp0"
@@ -11,9 +11,9 @@ bin\SDK_v2.3.0\BSOA.Demo.exe load "%SampleFilePath%"
 
 bin\SDK_v2.3.1\BSOA.Demo.exe load "%SampleFilePath%"
 
-bin\Release\netcoreapp3.1\BSOA.Demo.exe loadandsave "%SampleFilePath%" "%SampleOutBsoaPath%"
+bin\Release\netcoreapp3.1\BSOA.Demo.exe loadandsave "%SampleFilePath%" "%SampleOutBsoaPath%" 1
 
-bin\Release\netcoreapp3.1\BSOA.Demo.exe loadandsave "%SampleOutBsoaPath%" "%SampleOutJsonPath%" 10
+bin\Release\netcoreapp3.1\BSOA.Demo.exe load "%SampleOutBsoaPath%" "%SampleOutJsonPath%" 10
 
 ::bin\Release\netcoreapp3.1\BSOA.Demo.exe diagnostics "%SampleOutBsoaPath%"
 
