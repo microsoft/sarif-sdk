e2872fec05471c38a41818f89d73ae2300d7a404
Eddy Nakamura <ednakamu@microsoft.com>
2020-07-16 12:52:36 -0300

    updating order

--- src/Test.UnitTests.Sarif.Multitool/ExtensionsTests.cs
+++ src/Test.UnitTests.Sarif.Multitool/ExtensionsTests.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using FluentAssertions;
-
-using System;
 using System.Collections.ObjectModel;
 using System.Text;
+
+using FluentAssertions;
 
 using Xunit;
 
