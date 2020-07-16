f4a8b4136ab6631e7af3714a8f8b237949aa7fe4
Eddy Nakamura <ednakamu@microsoft.com>
2020-07-16 08:07:17 -0300

    adding more cases to unit test

--- src/Test.UnitTests.Sarif.Multitool/ExtensionsTests.cs
+++ src/Test.UnitTests.Sarif.Multitool/ExtensionsTests.cs
@@ -22,6 +22,9 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool
         public Tuple<ToolComponentReference, string, bool>[] ToolComponentTestList = new[]
         {
             new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = -1, Guid = "" }, "", true),
+            new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = -1, Guid = "" }, null, true),
+            new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = -1, Guid = null }, null, true),
+            new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = -1, Guid = null }, "", true),
             new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = -1, Guid = "774707BC-6949-4DB5-826D-9FC0E38BFDEE" }, "774707BC-6949-4DB5-826D-9FC0E38BFDEE", true),
             new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = -1, Guid = "774707BC-6949-4DB5-826D-9FC0E38BFDEF" }, "774707BC-6949-4DB5-826D-9FC0E38BFDEE", false),
             new Tuple<ToolComponentReference, string, bool>(new ToolComponentReference{Index = 2, Guid = "774707BC-6949-4DB5-826D-9FC0E38BFDEE" }, "774707BC-6949-4DB5-826D-9FC0E38BFDEE", false),
