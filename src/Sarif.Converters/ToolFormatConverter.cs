a7afb15ddf2ef7088be333bb033f8a86784318c8
Michael Fanning <mikefan@microsoft.com>
2020-07-23 16:44:34 -0700

    Fix typo in file.

--- src/Sarif.Converters/ToolFormatConverter.cs
+++ src/Sarif.Converters/ToolFormatConverter.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;ijfsd
+using System;
 using System.IO;
 using Microsoft.CodeAnalysis.Sarif.Writers;
 using Newtonsoft.Json;
