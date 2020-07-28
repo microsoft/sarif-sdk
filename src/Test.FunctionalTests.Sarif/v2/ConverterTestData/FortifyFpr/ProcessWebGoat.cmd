e0d93e4131e87281979818469f27dbc14d2222f1
Michael Fanning <mikefan@microsoft.com>
2020-07-27 21:08:26 -0700

    Update script to auto-gen drive letter based on script path.

--- src/Test.FunctionalTests.Sarif/v2/ConverterTestData/FortifyFpr/ProcessWebGoat.cmd
+++ src/Test.FunctionalTests.Sarif/v2/ConverterTestData/FortifyFpr/ProcessWebGoat.cmd
@@ -1,4 +1,4 @@
-set %DRIVE%=%DRIVE%
+set %DRIVE%=%~d0
 
 if exist *webgoat*.sarif (
 del /q *webgoat*.sarif
@@ -8,8 +8,8 @@ set MT=%DRIVE%\src\sarif\bld\bin\AnyCPU_Debug\Sarif.Multitool\netcoreapp3.1\Sari
 
 %MT% convert -t FortifyFpr %DRIVE%\src\sarif\src\Test.FunctionalTests.Sarif\v2\ConverterTestData\FortifyFpr\WebGoat.fpr -o WebGoat.fpr.sarif -f -p --normalize-for-github-dsp
 
-%MT% page WebGoat.fpr.sarif -i 0 -c 5 -o WebGoat.fpr.page.1.sarif -f 
-%MT% page WebGoat.fpr.sarif -i 5 -c 500 -o WebGoat.fpr.page.2.sarif -f 
+%MT% page WebGoat.fpr.sarif -i 0 -c 7 -o WebGoat.fpr.page.1.sarif -f 
+%MT% page WebGoat.fpr.sarif -i 7 -c 500 -o WebGoat.fpr.page.2.sarif -f 
 
 %MT% validate %DRIVE%\src\sarif\src\Test.FunctionalTests.Sarif\v2\ConverterTestData\FortifyFpr\WebGoat.fpr.page.1.sarif
 %MT% validate %DRIVE%\src\sarif\src\Test.FunctionalTests.Sarif\v2\ConverterTestData\FortifyFpr\WebGoat.fpr.page.2.sarif
