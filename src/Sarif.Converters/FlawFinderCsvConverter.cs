22d99acf056ea749988e3dd9645939f5bb1d4525
Larry Golding <lgolding@comcast.net>
2020-09-24 15:27:57 -0700

    Introduce NYI exception-throwing converter.

---
+++ src/Sarif.Converters/FlawFinderCsvConverter.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+
+namespace Microsoft.CodeAnalysis.Sarif.Converters
+{
+    public class FlawFinderCsvConverter : ToolFileConverterBase
+    {
+        public override string ToolName => ToolFormat.FlawFinderCsv;
+
+        public override void Convert(Stream input, IResultLogWriter output, OptionallyEmittedData dataToInsert)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Test.UnitTests.Sarif.Converters/FlawFinderCsvConverterTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.Converters
+{
+    public class FlawFinderCsvConverterTests
+    {
+        [Fact]
+        public void Converter_IsNotYetImplemented()
+        {
+            var converter = new FlawFinderCsvConverter();
+            Action action = () => converter.Convert(input: null, output: null, dataToInsert: OptionallyEmittedData.None);
+            action.Should().Throw<NotImplementedException>();
+        }
+    }
+}
