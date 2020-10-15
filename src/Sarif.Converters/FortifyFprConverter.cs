7b0a7c053b953525e6720c37cfc4ef67fd0c8f06
Eddy Nakamura <ednakamu@microsoft.com>
2020-10-15 18:29:57 -0300

    enforcing header

--- src/Sarif.Converters/FortifyFprConverter.cs
+++ src/Sarif.Converters/FortifyFprConverter.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Micro  soft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
--- src/Sarif/GlobalSuppressions.cs
+++ src/Sarif/GlobalSuppressions.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Diagnostics.CodeAnalysis;
 
 [module: SuppressMessage("Microsoft.Security.Xml", "CA3053:UseXmlSecureResolver", Scope = "member", Target = "Microsoft.CodeAnalysis.Sarif.PropertiesDictionary.#LoadFromXml(System.IO.Stream)")]
