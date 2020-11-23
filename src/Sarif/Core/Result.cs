fbb6828b613e64c92117f92650c438bf7bf3bb58
Eddy Nakamura <ednakamu@microsoft.com>
2020-11-23 16:55:46 -0300

    moving to right files, adding tests

--- src/Sarif/Core/Result.cs
+++ src/Sarif/Core/Result.cs
@@ -93,6 +93,23 @@ namespace Microsoft.CodeAnalysis.Sarif
             return new ReportingDescriptor() { Id = this.RuleId ?? this.Rule?.Id };
         }
 
+        public bool IsSuppressed()
+        {
+            if (this == null)
+            {
+                throw new ArgumentNullException(nameof(Result));
+            }
+
+            IList<Suppression> suppressions = this.Suppressions;
+            if (suppressions == null || suppressions.Count == 0)
+            {
+                return false;
+            }
+
+            return suppressions.Any(s => s.Status == SuppressionStatus.Accepted)
+                && !suppressions.Any(s => s.Status == SuppressionStatus.Rejected || s.Status == SuppressionStatus.UnderReview);
+        }
+
         private static ReportingDescriptor GetRuleByIndex(IList<ReportingDescriptor> rules, int ruleIndex)
         {
             if (rules == null)
