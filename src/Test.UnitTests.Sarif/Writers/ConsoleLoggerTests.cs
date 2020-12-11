91026901c8f40d247f9d2c1e48b718017ee11412
Michael Fanning <mikefan@microsoft.com>
2020-12-11 15:32:56 -0800

    Emit console output in the absence of valid intra-file locations.

--- src/Test.UnitTests.Sarif/Writers/ConsoleLoggerTests.cs
+++ src/Test.UnitTests.Sarif/Writers/ConsoleLoggerTests.cs
@@ -11,6 +11,49 @@ namespace Microsoft.CodeAnalysis.Sarif.Writers
     public class ConsoleLoggerTests
     {
         [Fact]
+        public void ConsoleLogger_EmitsMessageWithCharOffsetRegion()
+        {
+            string tool = Guid.NewGuid().ToString();
+            string message = Guid.NewGuid().ToString();
+            string uriText = Guid.NewGuid().ToString();
+
+            Uri uri = new Uri(uriText, UriKind.RelativeOrAbsolute);
+
+            var result = new Result
+            {
+                Level = FailureLevel.Error,
+                Message = new Message { Text = message },
+                Locations = new[]
+                {
+                    new Location
+                    {
+                         PhysicalLocation = new PhysicalLocation
+                         {
+                             ArtifactLocation = new ArtifactLocation
+                             {
+                                 Uri = uri
+                             },
+                             Region = new Region
+                             {
+                                CharOffset = 1,
+                                CharLength = 10
+                             }
+                         }
+                    }
+                }
+            };
+
+            var logger = new ConsoleLogger(verbose: false, toolName: tool)
+            {
+                CaptureOutput = true
+            };
+
+            logger.Log(null, result);
+
+            string expected = $"{tool} error {uriText}";
+        }
+
+        [Fact]
         public void ConsoleLogger_EmitsNotificationLocationMessageOrToolName()
         {
             string messageGuid = Guid.NewGuid().ToString();
@@ -47,7 +90,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Writers
             output.Should().NotContain(toolName);
             output.Should().Contain(messageGuid);
 
-            // In the absence of notificaiton locations, the tool name should be 
+            // In the absence of notification locations, the tool name should be 
             // present as part of the console out message.
             notification.Locations = null;
             output = ConsoleLogger.FormatNotificationMessage(notification, toolName);
