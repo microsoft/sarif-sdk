2442b67c7af4e6abb556a45568e311e3fd011770
Eddy Nakamura <eddynaka@gmail.com>
2021-01-22 20:01:12 -0300

    Relaxing GH1005 (#2248)
    * Relaxing GH1005
    
    * renaming variables and function
    
    * fixing misspelled word
    
    * removing null check
    
    Co-authored-by: Michael C. Fanning <michael.fanning@microsoft.com>

--- src/Sarif.Multitool.Library/Rules/GH1005.LocationsMustBeRelativeUrisOrFilePaths.cs
+++ src/Sarif.Multitool.Library/Rules/GH1005.LocationsMustBeRelativeUrisOrFilePaths.cs
@@ -31,6 +31,14 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
 
         public override bool EnabledByDefault => false;
 
+        private List<Uri> workingDirectoryUris;
+
+        protected override void Analyze(Run run, string runPointer)
+        {
+            this.workingDirectoryUris = new List<Uri>();
+            GetWorkingDirectoryUris(run);
+        }
+
         protected override void Analyze(Result result, string resultPointer)
         {
             if (!(result.Locations?.Any() == true))
@@ -68,6 +76,15 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
 
             if (uri.IsAbsoluteUri && uri.Scheme != "file")
             {
+                if (this.workingDirectoryUris.Any(workingDirectoryUri => uri
+                    .OriginalString
+                    .StartsWith(workingDirectoryUri.OriginalString, StringComparison.OrdinalIgnoreCase)))
+                {
+                    // We have a workingDirectoryUri and that is the beginnig of the current URI
+                    // And that is OK.
+                    return;
+                }
+
                 string uriPointer = locationPointer
                     .AtProperty(SarifPropertyName.PhysicalLocation)
                     .AtProperty(SarifPropertyName.ArtifactLocation)
@@ -80,6 +97,20 @@ namespace Microsoft.CodeAnalysis.Sarif.Multitool.Rules
                     uriPointer,
                     nameof(RuleResources.GH1005_LocationsMustBeRelativeUrisOrFilePaths_Error_Default_Text),
                     uri.OriginalString);
+            }
+        }
+
+        private void GetWorkingDirectoryUris(Run run)
+        {
+            if (run.Invocations?.Any() == true)
+            {
+                foreach (Invocation invocation in run.Invocations)
+                {
+                    if (invocation.WorkingDirectory?.Uri != null)
+                    {
+                        this.workingDirectoryUris.Add(invocation.WorkingDirectory?.Uri);
+                    }
+                }
             }
         }
     }
