31e4296a80e8c8c5c301ed1d01770088b5b78cbe
Eddy Nakamura <ednakamu@microsoft.com>
2021-04-16 11:42:12 -0300

    Exposing ResultLevelKind

---
+++ src/Sarif/Core/ResultLevelKind.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.CodeAnalysis.Sarif
+{
+    public class ResultLevelKind
+    {
+        private ResultKind? resultKind;
+
+        public ResultKind Kind
+        {
+            get
+            {
+                // If kind is absent, it SHALL default to "fail".
+                if (!resultKind.HasValue)
+                {
+                    return ResultKind.Fail;
+                }
+
+                // If level has any value other than "none" and kind is present, then kind SHALL have the value "fail".
+                if (Level != FailureLevel.None)
+                {
+                    return ResultKind.Fail;
+                }
+
+                return resultKind.Value;
+            }
+            set { resultKind = value; }
+        }
+
+        public FailureLevel Level { get; set; }
+    }
+}
