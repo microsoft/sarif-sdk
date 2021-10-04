fbdeb8ce57caca45a2b58fac1199d719e9ad48a9
Eddy Nakamura <ednakamu@microsoft.com>
2021-10-04 14:19:34 -0300

    Removing unused usings

--- src/Test.UnitTests.Sarif/SarifExtensionsTests.cs
+++ src/Test.UnitTests.Sarif/SarifExtensionsTests.cs
@@ -2,10 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-
-using Microsoft.CodeAnalysis.Sarif.Baseline.ResultMatching;
 
 using Xunit;
 
