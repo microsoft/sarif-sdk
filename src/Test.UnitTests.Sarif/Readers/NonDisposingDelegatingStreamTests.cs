a60b77b1394ea6f0b0bdd8a37eb7b591f2f59251
Eddy Nakamura <eddynaka@gmail.com>
2021-11-03 21:55:59 -0300

    Fixing Load when deferred is enabled (#2406)
    * Fixing Load when deferred is enabled
    
    * Using Stream.Synchronized, updating dispose method
    
    * We only need to create the streamreader if not deferred
    
    * Fixes (#2401)
    
    * Update JsonPositionedTextReader.cs
    
    * Update SarifLog.cs
    
    * Response to comments (#2405)
    
    * Response to comments
    
    Some formatting.
    Removes seek in ctor.
    Changes JsonPositionedTextReader(Stream) to be a factory method so we can enforce that the stream provided is seekable.
    Adds some unit tests for DelegatingStream.
    Moves DelegatingStream next to JsonPositionedTextReader since it is a helper class.
    Adds test cases for deferred collections reading from the new stream API.
    
    * Reduce redundant validation code in tests.
    
    * Fix dispose in nonseekable stream
    
    * Add JsonPositionedTextReaderTest for non seekable
    
    * Addressing style feedback.
    
    * Covering more blocks from DelegatingStream
    
    * Addressing PR feedback
    
    * Addressing PR feedback
    
    * Updating tests
    
    * Removing NonSeekableStream
    
    * Addressing PR feedback
    
    Co-authored-by: Gabe Stocco <98900+gfs@users.noreply.github.com>

---
+++ src/Test.UnitTests.Sarif/Readers/NonDisposingDelegatingStreamTests.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+using FluentAssertions;
+
+using Microsoft.CodeAnalysis.Test.UnitTests.Sarif.Readers;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.Readers
+{
+    public class NonDisposingDelegatingStreamTests
+    {
+        private const int position = 3;
+        private const string text = "Hello";
+
+        [Fact]
+        public void NonDisposingDelegatingStream_NullStream_ShouldThrowArgumentNullException()
+        {
+            Exception exception = Record.Exception(() => new NonDisposingDelegatingStream(null));
+            exception.Should().BeOfType(typeof(ArgumentNullException));
+        }
+
+        [Fact]
+        public void NonDisposingDelegatingStream_BasicRead()
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+            var delegatingStream = new NonDisposingDelegatingStream(memoryStream);
+
+            Assert.Equal(0, delegatingStream.Position);
+            Assert.Equal(memoryStream.Position, delegatingStream.Position);
+
+            using var streamReader = new StreamReader(delegatingStream);
+            Assert.Equal(text, streamReader.ReadToEnd());
+            Assert.Equal(text.Length, delegatingStream.Position);
+            Assert.Equal(memoryStream.Position, delegatingStream.Position);
+        }
+
+        [Fact]
+        public void NonDisposingDelegatingStream_BasicSeek_ShouldGenerateException()
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+            var delegatingStream = new NonDisposingDelegatingStream(memoryStream);
+
+            Assert.Equal(0, delegatingStream.Position);
+            Assert.Equal(memoryStream.Position, delegatingStream.Position);
+
+            Exception exception = Record.Exception(() => delegatingStream.Position = position);
+
+            exception.Should().BeOfType(typeof(NotImplementedException));
+        }
+
+        [Fact]
+        public void NonDisposingDelegatingStream_DontPerturbPositionOnCtor()
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+            memoryStream.Position = position;
+            var delegatingStream = new NonDisposingDelegatingStream(memoryStream);
+
+            Assert.Equal(position, delegatingStream.Position);
+            Assert.Equal(memoryStream.Position, delegatingStream.Position);
+        }
+
+        [Fact]
+        public void NonDisposingDelegatingStream_NonSeekable()
+        {
+            Stream stream = GenerateNonSeekableStream();
+            stream.CanSeek.Should().BeFalse();
+
+            var delegatingStream = new NonDisposingDelegatingStream(stream);
+            delegatingStream.CanSeek.Should().BeFalse();
+
+            Exception exception = Record.Exception(() => new StreamReader(delegatingStream));
+            exception.Should().BeOfType(typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void NonDisposingDelegatingStream_VerifyingDipose()
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+            var delegatingStream = new NonDisposingDelegatingStream(memoryStream);
+
+            delegatingStream.Dispose();
+            memoryStream.Length.Should().NotBe(0);
+
+            delegatingStream.Dispose();
+            memoryStream.Length.Should().NotBe(0);
+
+            delegatingStream.DisposeUnderlyingStream();
+
+            Exception exception = Record.Exception(() => memoryStream.Length.Should().NotBe(0));
+            exception.Should().BeOfType(typeof(ObjectDisposedException));
+        }
+
+        internal static Stream GenerateNonSeekableStream()
+        {
+            var memoryStream = new MemoryStream();
+
+            using var aes = Aes.Create();
+            aes.Key = new byte[]
+            {
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+
+            byte[] iv = aes.IV;
+            memoryStream.Write(iv, 0, iv.Length);
+
+            var cryptoStream = new CryptoStream(
+                memoryStream,
+                aes.CreateEncryptor(),
+                CryptoStreamMode.Write);
+
+            using var encryptWriter = new StreamWriter(cryptoStream);
+            encryptWriter.WriteLine("Hello World!");
+
+            return cryptoStream;
+        }
+    }
+}
