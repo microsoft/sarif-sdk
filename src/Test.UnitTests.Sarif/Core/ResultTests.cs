f9b28833fc36f0b0f4bdcd1ae6aec185d99a6034
Shaopeng <81775155+shaopeng-gh@users.noreply.github.com>
2021-11-14 20:58:09 -0800

    Add tests for Result.Level

--- src/Test.UnitTests.Sarif/Core/ResultTests.cs
+++ src/Test.UnitTests.Sarif/Core/ResultTests.cs
@@ -1,11 +1,16 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 using FluentAssertions;
 
 using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.CodeAnalysis.Sarif.Writers;
+
+using Newtonsoft.Json.Linq;
 
 using Xunit;
 
@@ -42,6 +47,95 @@ namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.Core
 
             result.Kind.Should().Be(ResultKind.Fail);
         }
+
+        [Fact]
+        public void Result_Level_WriteCorrectValue_GeneralCases()
+        {
+            Result result;
+            ReportingConfiguration defaultConfiguration;
+            ReportingConfiguration overrideConfiguration;
+
+            result = new Result() { Level = FailureLevel.Error };
+            Assert.True(WriteSarifThenReadLevelNode(result) == FailureLevel.Error);
+
+            result = new Result() { Level = FailureLevel.Note };
+            Assert.True(WriteSarifThenReadLevelNode(result) == FailureLevel.Note);
+
+            result = new Result() { Level = FailureLevel.None };
+            Assert.True(WriteSarifThenReadLevelNode(result) == FailureLevel.None);
+
+            result = new Result() { };
+            Assert.True(WriteSarifThenReadLevelNode(result) == null);
+
+            result = new Result() { Level = FailureLevel.Error };
+            defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Warning };
+            Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration) == FailureLevel.Error);
+
+            result = new Result() { Level = FailureLevel.Error };
+            defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Warning };
+            overrideConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Note };
+            Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration, overrideConfiguration) == FailureLevel.Error);
+        }
+
+        [Fact]
+        public void Result_Level_WriteCorrectValue_Warning()
+        {
+            Result result;
+            ReportingConfiguration defaultConfiguration;
+            ReportingConfiguration overrideConfiguration;
+
+            result = new Result() { Level = FailureLevel.Warning };
+            Assert.True(WriteSarifThenReadLevelNode(result) == FailureLevel.Warning);
+
+            result = new Result() { Level = FailureLevel.Warning };
+            defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Error };
+            Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration) == FailureLevel.Warning);
+
+            result = new Result() { Level = FailureLevel.Warning };
+            defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Error };
+            overrideConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Note };
+            Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration, overrideConfiguration) == FailureLevel.Warning);
+        }
+
+        [Fact]
+        public void Result_Level_WriteCorrectValue_NotSet()
+        {
+            Result result;
+            ReportingConfiguration defaultConfiguration;
+            ReportingConfiguration overrideConfiguration;
+
+            result = new Result() {};
+            Assert.True(WriteSarifThenReadLevelNode(result) == null);
+
+            result = new Result() {};
+            defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Error };
+            Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration) == null);
+
+            result = new Result() {};
+            defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Error };
+            overrideConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Note };
+            Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration, overrideConfiguration) == null);
+        }
+
+        //[Fact]
+        //public void Result_Level_WriteCorrectValue_Null()
+        //{
+        //    Result result;
+        //    ReportingConfiguration defaultConfiguration;
+        //    ReportingConfiguration overrideConfiguration;
+
+        //    result = new Result() { Level = null };
+        //    Assert.True(WriteSarifThenReadLevelNode(result) == null);
+
+        //    result = new Result() { Level = null };
+        //    defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Error };
+        //    Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration) == null);
+
+        //    result = new Result() { Level = null };
+        //    defaultConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Error };
+        //    overrideConfiguration = new ReportingConfiguration() { Enabled = true, Level = FailureLevel.Note };
+        //    Assert.True(WriteSarifThenReadLevelNode(result, defaultConfiguration, overrideConfiguration) == null);
+        //}
 
         [Fact]
         public void Result_TryIsSuppressed_ShouldReturnFalseWhenNoSuppressionsAreAvailable()
@@ -86,6 +180,57 @@ namespace Microsoft.CodeAnalysis.Test.UnitTests.Sarif.Core
             result.Suppressions.Add(new Suppression { Status = SuppressionStatus.Accepted });
             result.TryIsSuppressed(out isSuppressed).Should().BeTrue();
             isSuppressed.Should().BeTrue();
+        }
+
+        private static FailureLevel? WriteSarifThenReadLevelNode(Result result,
+            ReportingConfiguration defaultConfiguration = null, ReportingConfiguration overrideConfiguration = null)
+        {
+            const string sampleRuleId = "SampleRuleId";
+            const string sampleMessage = "Sample Message";
+            result.RuleId = sampleRuleId;
+            result.Message = new Message() { Text = sampleMessage };
+
+            List<ConfigurationOverride> ruleConfigurationOverrides = null;
+
+            if (overrideConfiguration != null)
+            {
+                var ruleConfigurationOverride = new ConfigurationOverride()
+                {
+                    Descriptor = new ReportingDescriptorReference()
+                    {
+                        Index = 0
+                    },
+                    Configuration = overrideConfiguration
+                };
+                ruleConfigurationOverrides = new List<ConfigurationOverride>() { ruleConfigurationOverride };
+            }
+
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+
+            using (var logger = new SarifLogger(
+                streamWriter,
+                logFilePersistenceOptions: LogFilePersistenceOptions.PrettyPrint,
+                dataToRemove: OptionallyEmittedData.NondeterministicProperties,
+                closeWriterOnDispose: false,
+                run: new Run() { Invocations = new List<Invocation> { new Invocation() { RuleConfigurationOverrides = ruleConfigurationOverrides } } },
+                levels: new List<FailureLevel> { FailureLevel.Error, FailureLevel.Warning, FailureLevel.Note, FailureLevel.None },
+                kinds: new List<ResultKind> { ResultKind.None, ResultKind.NotApplicable, ResultKind.Pass,
+                    ResultKind.Fail, ResultKind.Review, ResultKind.Open, ResultKind.Informational }))
+            {
+                logger.Log(new ReportingDescriptor { Id = sampleRuleId, DefaultConfiguration = defaultConfiguration }, result);
+            }
+
+            // Important. Force streamwriter to commit everything.
+            streamWriter.Flush();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            string memoryStreamText = streamWriter.Encoding.GetString(memoryStream.ToArray());
+            var jObject = JObject.Parse(memoryStreamText);
+            JToken levelJson = jObject["runs"][0]["results"][0]["level"];
+            string levelString = levelJson == null ? null : (string)levelJson;
+            FailureLevel? level = levelString == null ? null : (FailureLevel?)Enum.Parse(typeof(FailureLevel), levelString, ignoreCase: true);
+            return level;
         }
     }
 }
