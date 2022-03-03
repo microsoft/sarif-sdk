b2879a95ea3fde9cf96337697c68c51b6583cc8f
Yong Yan <yanyong-gh@outlook.com>
2022-03-02 17:55:05 -0800

    Add config to avoid false positive ESlint alerts

--- src/ESLint.Formatter/.eslintrc.js
+++ src/ESLint.Formatter/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "browser": true,
         "commonjs": true,
         "es2021": true,
-        "node": true
+        "node": true,
+        "mocha": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
