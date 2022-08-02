bca52ca32e401f2b9b88171c7301d7d862ac025b
Yong Yan <yanyong-gh@outlook.com>
2022-08-02 14:24:11 -0700

    Remove unused using.

--- src/Sarif.Multitool.Library/MergeCommand.cs
+++ src/Sarif.Multitool.Library/MergeCommand.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
