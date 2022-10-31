1fab52749af4663bc74d57b649833df5b87f4f2a
Michael C. Fanning <mikefan@microsoft.com>
2022-10-31 14:55:29 -0700

    Expose internal data for unit-testing. (#2566)

--- src/Sarif/FileRegionsCache.cs
+++ src/Sarif/FileRegionsCache.cs
@@ -17,7 +17,7 @@ namespace Microsoft.CodeAnalysis.Sarif
         public static readonly FileRegionsCache Instance = new FileRegionsCache();
         public const int DefaultCacheCapacity = 100;
         private readonly IFileSystem _fileSystem;
-        private readonly Cache<string, Tuple<string, NewLineIndex>> _cache;
+        internal readonly Cache<string, Tuple<string, NewLineIndex>> _cache;
 
         /// <summary>
         /// Creates a new <see cref="FileRegionsCache"/> object.
