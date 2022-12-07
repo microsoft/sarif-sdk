d1a8b184f6f0eb0c2a2f760b9362fdca7094aaf5
Shaopeng Li <v-shaopengli@microsoft.com>
2022-12-07 10:53:33 -0800

    replace { and }

--- src/Sarif/Visitors/VisitorHelper.cs
+++ src/Sarif/Visitors/VisitorHelper.cs
@@ -3,6 +3,7 @@
 
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.CodeAnalysis.Sarif.Visitors
 {
@@ -10,23 +11,29 @@ namespace Microsoft.CodeAnalysis.Sarif.Visitors
     {
         public static void FlattenMessage(Message node, int ruleIndex, Run run)
         {
-            ReportingDescriptor rule = ruleIndex != -1 ? run.Tool.Driver.Rules[ruleIndex] : null;
+            ReportingDescriptor rule = ruleIndex != -1 ? run?.Tool?.Driver?.Rules?[ruleIndex] : null;
 
-            if (rule != null &&
-                rule.MessageStrings != null &&
-                rule.MessageStrings.TryGetValue(node.Id, out MultiformatMessageString formatString))
-            {
-                node.Text = node.Arguments?.Count > 0
-                    ? rule.Format(node.Id, node.Arguments)
-                    : formatString?.Text;
-            }
-
-            if (node.Text == null &&
-                run.Tool.Driver.GlobalMessageStrings?.TryGetValue(node.Id, out formatString) == true)
+            if (rule?.MessageStrings != null &&
+                rule.MessageStrings.TryGetValue(node.Id, out MultiformatMessageString formatString) == true)
             {
                 node.Text = node.Arguments?.Count > 0 && formatString != null
                     ? string.Format(CultureInfo.CurrentCulture, formatString.Text, node.Arguments.ToArray())
                     : formatString?.Text;
+            }
+
+            if (node.Text == null &&
+                run?.Tool?.Driver?.GlobalMessageStrings != null &&
+                run.Tool.Driver.GlobalMessageStrings.TryGetValue(node.Id, out formatString) == true)
+            {
+                node.Text = node.Arguments?.Count > 0 && formatString != null
+                    ? string.Format(CultureInfo.CurrentCulture, formatString.Text, node.Arguments.ToArray())
+                    : formatString?.Text;
+            }
+
+            if (node.Text != null)
+            {
+                node.Text = Regex.Replace(node.Text, "{+", m => m.Value.Length == 1 ? "{{" : m.Value);
+                node.Text = Regex.Replace(node.Text, "}+", m => m.Value.Length == 1 ? "}}" : m.Value);
             }
         }
     }
