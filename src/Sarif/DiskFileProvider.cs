2ba2bff90ad0d30f738e2f61f2ea1d164e8a52bf
Michael C. Fanning <mikefan@microsoft.com>
2023-01-13 18:08:46 -0800

    Add files

---
+++ src/Sarif/DiskFileProvider.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Microsoft.CodeAnalysis.Sarif.Driver
+{
+    public class DiskFileProvider : IArtifactProvider
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public DiskFileProvider(string specifier, bool recurse = false, IFileSystem fileSystem = null)
+        {
+            _specifier = specifier;
+            _recurse = recurse;
+            _fileSystem = fileSystem ?? FileSystem.Instance;
+        }
+
+        private readonly bool _recurse;
+        private readonly string _specifier;
+        private List<string> _files;
+        private List<string> _directories;
+
+        public IList<string> Files
+        {
+            get { return _files ?? BuildFiles(); }
+        }
+
+        public IList<string> Directories
+        {
+            get { return _directories ?? BuildDirectories(); }
+        }
+
+        public IEnumerable<IArtifact> Artifacts => throw new NotImplementedException();
+
+        private List<string> BuildDirectories()
+        {
+            BuildFilesAndDirectoriesList();
+            return _directories;
+        }
+
+        private List<string> BuildFiles()
+        {
+            BuildFilesAndDirectoriesList();
+            return _files;
+        }
+
+        private void BuildFilesAndDirectoriesList()
+        {
+            string expandedSpecifier;
+
+            _files = new List<string>();
+            _directories = new List<string>();
+
+            expandedSpecifier = Environment.ExpandEnvironmentVariables(_specifier);
+
+            string filter = Path.GetFileName(expandedSpecifier);
+            string directory = Path.GetDirectoryName(expandedSpecifier);
+
+            if (directory.Length == 0)
+            {
+                directory = $".{Path.DirectorySeparatorChar}";
+            }
+
+            AddFilesFromDirectory(directory, filter);
+        }
+
+        private void AddFilesFromDirectory(string dir, string filter)
+        {
+            if (_fileSystem.DirectoryExists(dir))
+            {
+                foreach (string file in _fileSystem.DirectoryGetFiles(dir, filter))
+                {
+                    AddFileToList(file);
+                }
+
+                if (_recurse)
+                {
+                    try
+                    {
+                        foreach (string subdir in _fileSystem.DirectoryGetDirectories(dir))
+                        {
+                            AddFilesFromDirectory(subdir, filter);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.Error.WriteLineAsync("Unauthorized access exception for directory: " + dir);
+                    }
+                }
+            }
+        }
+
+        private void AddFileToList(string expandedSpecifier)
+        {
+            _files.Add(Path.GetFullPath(expandedSpecifier));
+        }
+    }
+}
---
+++ src/Sarif/IFileProvider.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.Sarif
+{
+    public interface IArtifactProvider
+    {
+        IEnumerable<IArtifact> Artifacts { get; }
+    }
+
+    public interface IArtifact
+    {
+        Uri Uri { get; set; }
+
+        string Text { get; set;  }
+
+        Encoding Encoding { get; set; }
+    }
+}
