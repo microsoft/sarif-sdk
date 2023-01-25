573291a73e69159ca8710e9de2319df56bd3d88e
Suvam Mukherjee <sumukherjee@microsoft.com>
2023-01-25 15:42:55 -0500

    format fixing Long

--- src/Sarif/Numeric/Long.cs
+++ src/Sarif/Numeric/Long.cs
@@ -2,8 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Microsoft.CodeAnalysis.Sarif.Numeric
 {
