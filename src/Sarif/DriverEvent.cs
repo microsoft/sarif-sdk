fafcf9832f1f731558c2cc753a3c9ab400017756
Michael C. Fanning <mikefan@microsoft.com>
2023-05-12 11:10:57 -0700

    Driver ETW events (#2668)
    * Preliminary work to implement built-in ETW events for key outcomes.
    
    * Update provider name. Update release notes.
    
    * Update release notes with PR info.
    
    * Uncomment rule reserved stop event.
    
    * Refine tests.

---
+++ src/Sarif/DriverEvent.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.CodeAnalysis.Sarif
+{
+    public enum DriverEvent : int
+    {
+        EnumerateTargetsStart = 1,
+        EnumerateTargetsStop = 2,
+        ArtifactSizeInBytes = 3,
+        GetTargetStart = 4,
+        GetTargetStop = 5,
+        ScanTargetStart = 6,
+        ScanTargetStop = 7,
+        RuleStart = 8,
+        RuleStop = 9,
+        RuleFired = 10,
+        LogResultsStart = 11,
+        LogResultsEnd = 12,
+        RuleReserved = 13,
+        RuleReservedStart = 14,
+        RuleReservedStop = 15,
+        TargetReserved = 16,
+        TargetReservedStart = 17,
+        TargetReservedStop = 18,
+    }
+}
