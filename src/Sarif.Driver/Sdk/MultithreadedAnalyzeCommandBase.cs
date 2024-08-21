a0b950d28f42cd80207fca3efd41b224ac02611c
Shaopeng Li <v-shaopengli@microsoft.com>
2024-08-21 01:27:39 -0700

    Add tests

--- src/Sarif.Driver/Sdk/MultithreadedAnalyzeCommandBase.cs
+++ src/Sarif.Driver/Sdk/MultithreadedAnalyzeCommandBase.cs
@@ -654,7 +654,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Driver
             {
                 globalContext.CancellationToken.ThrowIfCancellationRequested();
 
-                string filePath = artifact.Uri.OriginalString;
+                string filePath = artifact.Uri.GetFilePath();
 
                 if (globalContext.CompiledGlobalFileDenyRegex?.Match(filePath).Success == true)
                 {
