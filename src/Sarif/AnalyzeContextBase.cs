49006a8dbfab6dda7530a0d9a97c1c72950735e7
Aasim Malladi <aamallad@microsoft.com>
2026-01-27 15:00:38 -0800

    Set default to false

--- src/Sarif/AnalyzeContextBase.cs
+++ src/Sarif/AnalyzeContextBase.cs
@@ -76,7 +76,7 @@ namespace Microsoft.CodeAnalysis.Sarif
         public virtual IAnalysisLogger Logger { get; set; }
         public virtual RuntimeConditions RuntimeErrors { get; set; }
         public virtual bool AnalysisComplete { get; set; }
-        public virtual bool ThreadSafeLogging { get; set; } = true;
+        public virtual bool ThreadSafeLogging { get; set; } = false;
         public bool Inline => OutputFileOptions.HasFlag(FilePersistenceOptions.Inline);
         public bool Minify => OutputFileOptions.HasFlag(FilePersistenceOptions.Minify);
         public bool Optimize => OutputFileOptions.HasFlag(FilePersistenceOptions.Optimize);
